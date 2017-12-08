b3661c28030f830a71fb0639f0aed0d3fce10303
Clement Denis <le.mikmac@gmail.com>
2017-12-08 10:00:35 +0000

    fiw sentence test

--- sentence.js
+++ sentence.js
@@ -11,10 +11,10 @@
 //* Begin of tests
 const assert = require('assert')
 
-assert.strictEqual(typeof words, 'function')
-assert.strictEqual(words.length, 1)
-assert.deepStrictEqual(words([ 'a', 'b', 'c' ]), 'a b c')
-assert.deepStrictEqual(words([ 'a', '1', 'c' ]), 'a 1 c')
-assert.deepStrictEqual(words([ 'a', '1', 'c', 'd', 'e', 'f' ]), 'a 1 c d e f')
-assert.deepStrictEqual(words([ 'wol.lol', 'lo,lol']), 'wol.lol lo,lol')
+assert.strictEqual(typeof sentence, 'function')
+assert.strictEqual(sentence.length, 1)
+assert.deepStrictEqual(sentence([ 'a', 'b', 'c' ]), 'a b c')
+assert.deepStrictEqual(sentence([ 'a', '1', 'c' ]), 'a 1 c')
+assert.deepStrictEqual(sentence([ 'a', '1', 'c', 'd', 'e', 'f' ]), 'a 1 c d e f')
+assert.deepStrictEqual(sentence([ 'wol.lol', 'lo,lol']), 'wol.lol lo,lol')
 // End of tests */
