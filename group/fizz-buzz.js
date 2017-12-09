771ef509a3b0c75cf6ccf571741de4d6888add5f
Clement Denis <le.mikmac@gmail.com>
2017-12-09 15:20:20 +0000

    group work

---
+++ group/fizz-buzz.js
@@ -0,0 +1,22 @@
+'use strict'
+
+/*
+ * Create function `fizzBuzz` that print numbers from 1 to 100
+ * But for multiples of three, print "Fizz” instead of the number
+ * and for the multiples of five, print "Buzz".
+ * For numbers which are multiples of both three and five, print "FizzBuzz".
+ *
+ * use console.log to print
+ *
+ * Français:
+ * Créer la fonction `FizzBuzz` qui affiche les nombres de 1 à 100
+ * Mais pour les multiples de trois, affiche "Fizz" à la place du nombre
+ * et pour les multiples de cinq, affiche "Buzz".
+ * Pour les nombres qui sont des multiples de trois et de cinq, affiche "FizzBuzz".
+ *
+ * Utilisez console.log pour afficher.
+ */
+
+
+// You must write your own tests
+throw Error('No tests !')
---
+++ group/price-is-right.js
@@ -0,0 +1,18 @@
+'use strict'
+
+/*
+ * Price is right
+ * 1 can of soup is 650 FCFA
+ * 3 can of soup is 1000 FCFA
+ * 1 can of soup contains 500 ml
+ *
+ * Create a function `getCostCount` that takes a number of cans
+ * and return the cost (and applying the 3 for 1000 FCFA discount)
+ *
+ * Create a function `getCostMl` that takes a quantity in milliliters
+ * and return the cost (and applying the 3 for 1000 FCFA discount)
+ */
+
+
+// You must write your own tests
+throw Error('No tests !')
