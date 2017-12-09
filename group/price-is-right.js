d8f67902a0ab3a59d56bb064e9d43fdd4aa81cf9
Clement Denis <le.mikmac@gmail.com>
2017-12-09 16:38:22 +0000

    change price

--- group/price-is-right.js
+++ group/price-is-right.js
@@ -3,14 +3,14 @@
 /*
  * Price is right
  * 1 can of soup is 650 FCFA
- * 3 can of soup is 1000 FCFA
+ * 3 can of soup is 1500 FCFA
  * 1 can of soup contains 500 ml
  *
  * Create a function `getCostCount` that takes a number of cans
- * and return the cost (and applying the 3 for 1000 FCFA discount)
+ * and return the cost (and applying the 3 for 1500 FCFA discount)
  *
  * Create a function `getCostMl` that takes a quantity in milliliters
- * and return the cost (and applying the 3 for 1000 FCFA discount)
+ * and return the cost (and applying the 3 for 1500 FCFA discount)
  */
 
 
