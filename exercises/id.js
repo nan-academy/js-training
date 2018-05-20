43508f4b8dd4ca89fa26d53a95685dae2000dcc0
Jérôme Legrand <jerome.legrand4537@gmail.com>
2018-05-20 21:02:22 +0200

    réalisation des test basics

--- exercises/id.js
+++ exercises/id.js
@@ -7,6 +7,9 @@
  */
 
 // Your code :
+function id(id) {
+    return id;
+}
 
 //* Begin of tests
 const assert = require('assert')
--- exercises/primitives.js
+++ exercises/primitives.js
@@ -8,7 +8,10 @@
  */
 
 // Your code :
-
+const str = '1337';
+const num = 42;
+const bool = false;
+const undef = undefined;
 
 //* Begin of tests
 const assert = require('assert')
