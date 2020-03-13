de2c4ee2a31ea87f2497d47d32b5ca92a0f3b2f5
Clement Denis <le.mikmac@gmail.com>
2020-03-13 22:45:14 +0000

    content: abs subject

--- exercise/abs.js
+++ exercise/abs.js
@@ -1,22 +1,28 @@
 /*
- * Create the `abs` function that takes one number argument
- * and returns its absolute value.
- * You are not allowed to use `Math.abs`, make your own.
- * see: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Math/abs
- *
- * @notions Functions, Ternary
- */
+## Abs(olute)
 
-// Your code :
+### Instruction
 
-const assert = require('assert')
+Create the `abs` function that takes one number argument
+and returns its absolute value.
+You are not allowed to use `Math.abs`, make your own.
+see: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Math/abs
 
-assert.strictEqual(typeof abs, 'function')
-assert.strictEqual(abs.toString().includes('Math.abs'), false)
-assert.notStrictEqual(abs, Math.abs)
-assert.strictEqual(abs.length, 1)
-assert.strictEqual(abs(0), 0)
-assert.strictEqual(abs(-1), 1)
-assert.strictEqual(abs(-13.2), 13.2)
-assert.strictEqual(abs(132), 132)
-// End of tests */
+### Notions
+
+- Functions
+- Ternary
+*/
+
+
+export const tests = []
+const t = f => tests.push(f)
+
+t(() => typeof abs === 'function')
+t(() => abs.toString().includes('Math.abs') === false)
+t(() => abs !== Math.abs)
+t(() => abs.length === 1)
+t(() => abs(0) === 0)
+t(() => abs(-1) === 1)
+t(() => abs(-13.2) === 13.2)
+t(() => abs(132) === 132)
