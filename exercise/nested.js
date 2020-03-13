914411bfaab2eb0f018a91925414389ec0d70e6f
Clement Denis <le.mikmac@gmail.com>
2020-03-13 23:04:41 +0000

    content: nested fix typo in subject

--- exercise/nested.js
+++ exercise/nested.js
@@ -7,7 +7,7 @@ Create a `nested` constant variable that contains
 - arr: an array of the 3 values: 4, undefined, '2'
 - obj: an object with 3 property (str, num & bool)
 
-`nested` and `obj` must be frozen to prevent changes form them.
+`nested` and `obj` must be frozen to prevent changes from them.
 
 ### Notions
 
