cba08c72a9affbec1787ea0110425228091a6c5c
Clement Denis <le.mikmac@gmail.com>
2020-03-15 01:24:46 +0000

    add entry to introduction module

--- module/hello.js
+++ module/hello.js
@@ -1,5 +1,7 @@
 export const next = 'data'
 export const timeout = 2500 // default is 100 per tests
 export const exercises = {
-  'browser-introduction': {}
+  'introduction': {},
+  'console': {},
 }
+
