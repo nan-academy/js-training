a273eeb19f1280631ef594a95f857755be4872c1
MSilva95 <miguel-silva98@hotmail.com>
2020-03-26 18:40:09 +0000

    adding to module

--- module/find-your-timing.js
+++ module/find-your-timing.js
@@ -5,8 +5,9 @@ export const exercises = {
   // date
   // find most usefull functions from https://date-fns.org/docs/Getting-Started
 
+  emailType: { feedback: true },
   'count-leap-years': { feedback: true },
   'day-of-the-year': { feedback: true },
   'bloody-sunday': { feedback: true },
-  'unicode-technical-report-35': { feedback: true },
+  'unicode-technical-report-35': { feedback: true }
 }
