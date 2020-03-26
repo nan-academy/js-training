aadc61bf11e58524de38d75f57ed68c1d7d49026
MSilva95 <miguel-silva98@hotmail.com>
2020-03-26 16:31:44 +0000

    adding new exercise for regex

--- exercise/emailType.js
+++ exercise/emailType.js
@@ -9,17 +9,34 @@ and the group where the type is (@gmail).
 
 ### Notions
 
-///*/// ⚡
+///*/ // ⚡
 
 ///*/// ⚡
 export const tests = []
 const t = f => tests.push(f)
 t(() => typeof emailType === 'function')
 t(() => emailType.length === 1)
-t(({eq}) => eq(emailType('this is my gmail mygmail@gmail.com use it well'), ['mygmail@gmail.com', '@gmail'] ))
-t(({eq}) => eq(emailType('this is a test ex-01@hotmail.com'), ['ex-01@hotmail.com','@hotmail'] ))
-t(({eq}) => eq(emailType('rfbgvdm ms-ma@hotmail.com xz mail.com format'), ['ms-ma@hotmail.com','@hotmail'] ))
-t(({eq}) => eq(emailType('a@adv adva-sa@live.com'), ['adva-sa@live.com','@live'] ))
-t(({eq}) => eq(emailType('a@adv advas sdfb '), [] ))
+t(({ eq }) =>
+  eq(emailType('this is my gmail mygmail@gmail.com use it well'), [
+    'mygmail@gmail.com',
+    '@gmail'
+  ])
+)
+t(({ eq }) =>
+  eq(emailType('this is a test ex-01@hotmail.com'), [
+    'ex-01@hotmail.com',
+    '@hotmail'
+  ])
+)
+t(({ eq }) =>
+  eq(emailType('rfbgvdm ms-ma@hotmail.com xz mail.com format'), [
+    'ms-ma@hotmail.com',
+    '@hotmail'
+  ])
+)
+t(({ eq }) =>
+  eq(emailType('a@adv adva-sa@live.com'), ['adva-sa@live.com', '@live'])
+)
+t(({ eq }) => eq(emailType('a@adv advas sdfb '), []))
 
 Object.freeze(tests)
--- solution/emailType.js
+++ solution/emailType.js
@@ -9,27 +9,44 @@ and the group where the type is (@gmail).
 
 ### Notions
 
-///*/// ⚡
+///*/ // ⚡
 function emailType(str) {
-    let arr = []
-    var myRegexp = /\w+(?:[\.-]?\w+)*(@\w+)(?:[\.-]?\w+)*(?:\.\w{2,3})/g
-    var match = myRegexp.exec(str)
-    if (match != null) {
-        arr.push(match[0])
-        arr.push(match[1])
-        return (arr)
-    }
+  let arr = []
+  var myRegexp = /\w+(?:[\.-]?\w+)*(@\w+)(?:[\.-]?\w+)*(?:\.\w{2,3})/g
+  var match = myRegexp.exec(str)
+  if (match != null) {
+    arr.push(match[0])
+    arr.push(match[1])
     return arr
   }
-  ///*/// ⚡
-  export const tests = []
-  const t = f => tests.push(f)
-  t(() => typeof emailType === 'function')
-  t(() => emailType.length === 1)
-  t(({eq}) => eq(emailType('this is my gmail mygmail@gmail.com use it well'), ['mygmail@gmail.com', '@gmail'] ))
-  t(({eq}) => eq(emailType('this is a test ex-01@hotmail.com'), ['ex-01@hotmail.com','@hotmail'] ))
-  t(({eq}) => eq(emailType('rfbgvdm ms-ma@hotmail.com xz mail.com format'), ['ms-ma@hotmail.com','@hotmail'] ))
-  t(({eq}) => eq(emailType('a@adv adva-sa@live.com'), ['adva-sa@live.com','@live'] ))
-  t(({eq}) => eq(emailType('a@adv advas sdfb '), [] ))
-  
-  Object.freeze(tests)
+  return arr
+}
+///*/// ⚡
+export const tests = []
+const t = f => tests.push(f)
+t(() => typeof emailType === 'function')
+t(() => emailType.length === 1)
+t(({ eq }) =>
+  eq(emailType('this is my gmail mygmail@gmail.com use it well'), [
+    'mygmail@gmail.com',
+    '@gmail'
+  ])
+)
+t(({ eq }) =>
+  eq(emailType('this is a test ex-01@hotmail.com'), [
+    'ex-01@hotmail.com',
+    '@hotmail'
+  ])
+)
+t(({ eq }) =>
+  eq(emailType('rfbgvdm ms-ma@hotmail.com xz mail.com format'), [
+    'ms-ma@hotmail.com',
+    '@hotmail'
+  ])
+)
+t(({ eq }) =>
+  eq(emailType('a@adv adva-sa@live.com'), ['adva-sa@live.com', '@live'])
+)
+t(({ eq }) => eq(emailType('a@adv advas sdfb '), []))
+
+Object.freeze(tests)
