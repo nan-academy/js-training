6fdb7537aa90165f2be75949b02069a3f1bc0265
Christopher Fremond <christopher.fremond@gmail.com>
2020-03-27 11:19:17 +0000

    chris solutions safeguarding

---
+++ frenchris/mutability.js
@@ -0,0 +1,6 @@
+let clone1 = Object.assign(person)
+let clone2 = Object.assign(person)
+
+let samePerson = Object.assign(person)
+
+person.age = 78
---
+++ frenchris/nested.js
@@ -0,0 +1,11 @@
+const nested = {
+  arr: [4, undefined, '2'],
+  obj: {
+    str: '',
+    num: 4,
+    bool: true
+  }
+}
+
+Object.freeze(nested)
+Object.freeze(nested.obj)
