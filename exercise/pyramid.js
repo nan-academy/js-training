e1de36b051ead9b241fbb3486653962c094ba5b3
Clement Denis <le.mikmac@gmail.com>
2020-03-27 12:14:55 +0100

    add params to the test runner

--- exercise/pyramid.js
+++ exercise/pyramid.js
@@ -29,14 +29,15 @@ Just like triangle
 // /*/ // ⚡
 export const tests = []
 const t = (f) => tests.push(f)
-t((_, { code }) => code.split('\n').filter((s) => s.trim()).length < 38)
+t(({ code }) => code.split('\n').filter((s) => s.trim()).length < 38)
+t(({ code }) => !code.includes('$'))
 
-t(() => pyramid('a', 5) === _5.slice(1))
-t(() => pyramid('+', 10) === _10.slice(1))
-t(() => pyramid('#', 40) === _40.slice(1))
-t(() => pyramid('{}', 12) === _12.slice(1))
-t(() => pyramid('ABC', 7) === _7.slice(1))
-t(() => pyramid('<^>', 13) === _13.slice(1))
+t(() => pyramid('a', 5) === $5.slice(1))
+t(() => pyramid('+', 10) === $10.slice(1))
+t(() => pyramid('#', 40) === $40.slice(1))
+t(() => pyramid('{}', 12) === $12.slice(1))
+t(() => pyramid('ABC', 7) === $7.slice(1))
+t(() => pyramid('<^>', 13) === $13.slice(1))
 
 Object.freeze(tests)
 
--- node-runner.js
+++ node-runner.js
@@ -4,21 +4,17 @@ import fs from 'fs'
 import { deepStrictEqual } from 'assert'
 import { run } from './lib/tester.js'
 const { readFile } = fs.promises
-
-const args = process.argv.slice(2)
-if (args.length) {
   
-} else {
-  run('data', {
-    eq: (a, b) => (deepStrictEqual(a, b), true),
-    readFileOrEmpty: path => readFile(path, 'utf8').then(
-      str => str.replace(/\r\n/g, '\n'),
-      err => err.code === 'ENOENT' ? '' : Promise.reject(err)
-    ),
-    importCode: code => {
-      const b64 = Buffer.from(code).toString('base64')
-      return import(`data:text/javascript;base64,${b64}`)
-    },
-  })
-    .catch(err => (process.stdout.write(err.stack), process.exit(1)))
-}
+run({
+  args: process.argv.slice(2),
+  eq: (a, b) => (deepStrictEqual(a, b), true),
+  readFileOrEmpty: path => readFile(path, 'utf8').then(
+    str => str.replace(/\r\n/g, '\n'),
+    err => err.code === 'ENOENT' ? '' : Promise.reject(err)
+  ),
+  importCode: code => {
+    const b64 = Buffer.from(code).toString('base64')
+    return import(`data:text/javascript;base64,${b64}`)
+  },
+})
+  .catch(err => (process.stdout.write(err.stack), process.exit(1)))
