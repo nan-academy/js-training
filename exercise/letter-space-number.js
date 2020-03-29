c1d8aa2279f7a55e1a6b5492689d30cd035817e2
Clement Denis <le.mikmac@gmail.com>
2020-03-29 14:23:19 +0200

    fixing solutions and subjects

--- exercise/letter-space-number.js
+++ exercise/letter-space-number.js
@@ -26,7 +26,7 @@ t(({eq}) =>
 t(({eq}) => eq(letterSpaceNumber("I like 7up."), []))
 t(({eq}) => eq(letterSpaceNumber("It's 20 past 3"), ["t 3"]))
 t(({eq}) => eq(letterSpaceNumber("example 1, example 2"), ["e 1", "e 2"]))
-t(({eq}) => eq(letterSpaceNumber(""), ["e 1", "e 2"]))
+t(({eq}) => eq(letterSpaceNumber(""), []))
 t(({eq}) => eq(letterSpaceNumber("Definitely 9."), ["y 9"]))
 
 Object.freeze(tests)
--- exercise/vowel-dots.js
+++ exercise/vowel-dots.js
@@ -5,20 +5,17 @@
 
 Create a function called vowelDots that receives a string and adds a `.` after
 every vowel ('y' is not considered a vowel here) using a regex called `vowels`.
- */
 
 // /*/ // ⚡
 
 // /*/ // ⚡
-
 export const tests = []
-const t = f => tests.push(f)
+const t = (f) => tests.push(f)
 
-t(({eq}) => eq(typeof vowels, "object"))
-t(({eq}) => eq(vowelDots("something"), "so.me.thi.ng"))
-t(({eq}) => eq(vowelDots(""), ""))
-t(({eq}) => eq(vowelDots("rhythm"), "rhythm"))
-t(({eq}) => eq(vowelDots("Algorithm"), "A.lgo.ri.thm"))
-
+t(({ eq }) => vowels.test('a') && !vowels.test('c'))
+t(({ eq }) => eq(vowelDots('something'), 'so.me.thi.ng'))
+t(({ eq }) => eq(vowelDots(''), ''))
+t(({ eq }) => eq(vowelDots('rhythm'), 'rhythm'))
+t(({ eq }) => eq(vowelDots('Algorithm'), 'A.lgo.ri.thm'))
 
 Object.freeze(tests)
