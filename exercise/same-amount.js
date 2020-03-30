beec155746a6f5c0f62591bba392b92e5c5efd9a
Clement Denis <le.mikmac@gmail.com>
2020-03-30 18:02:27 +0200

    make so that same-amount use regexes

--- exercise/same-amount.js
+++ exercise/same-amount.js
@@ -11,7 +11,7 @@ The objective is to confirm that the second and the third parameter occurred
 
 ### Notions
 
-- https://github.com/ziishaned/learn-regex#4-lookarounds
+- https://developer.mozilla.org/en-US/docs/Web/JavaScript/Guide/Regular_Expressions#Using_regular_expressions_in_JavaScript
 
 // /*/ // ⚡
 
@@ -19,19 +19,21 @@ The objective is to confirm that the second and the third parameter occurred
 export const tests = []
 const t = f => tests.push(f)
 
-t(() => sameAmount('hello how are you', 'l', 'e'))
-t(() => sameAmount('hello how are you', 'h', 'e'))
-t(() => sameAmount('hello how are you', 'he', 'ho'))
+t(() => sameAmount('hello how are you', /l/, /e/))
+t(() => sameAmount('hello how are you', /h/, /e/))
+t(() => sameAmount('hello how are you', /he/, /ho/))
 
-let data =
-  'qqqqqqq q qqqqqqqqqqqq q qq  qw w wq wqw  wqw  ijnjjnfapsdbjnkfsdiqw klfsdjn fs fsdnjnkfsdjnk sfdjn fsp fd'
-t(() => sameAmount(data, 'i', 'p'))
-t(() => !sameAmount(data, 'h', 'w'))
-t(() => sameAmount(data, 'qqqq ', 'qqqqqqq'))
-t(() => !sameAmount(data, 'q ', 'qqqqqqq'))
-t(() => sameAmount(data, 'fs', 'q '))
-t(() => !sameAmount(data, 'fs', 'q'))
-t(() => sameAmount(data, 'j', 'w'))
-t(() => !sameAmount(data, 'j', ''))
+t(() => sameAmount(data, /i/, /p/))
+t(() => !sameAmount(data, /h/, /w/))
+t(() => sameAmount(data, /qqqq /, /qqqqqqq/))
+t(() => !sameAmount(data, /q /, /qqqqqqq/))
+t(() => sameAmount(data, /fs[^q]/, /q /))
+t(() => sameAmount(data, /^[qs]/, /^[gq]/))
+t(() => sameAmount(data, /j/, /w/))
+t(() => !sameAmount(data, /j/, / /))
+t(() => sameAmount(data, /fs./, /jn./))
 
 Object.freeze(tests)
+
+const data = `qqqqqqq q qqqqqqqfsqqqqq q qq  qw w wq wqw  wqw
+ ijnjjnfapsdbjnkfsdiqw klfsdjn fs fsdnjnkfsdjnk sfdjn fsp fd`
--- tests/same-amount/pass_lee.js
+++ tests/same-amount/pass_lee.js
@@ -1,6 +1,4 @@
 const sameAmount = (str, first, second) => {
-  let reg = new RegExp(first, 'g')
-  let reg1 = new RegExp(second, 'g')
   let size = str.split(reg).length
   let size1 = str.split(reg1).length
   return size === size1
