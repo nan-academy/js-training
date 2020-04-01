846c1a32f8105ebe01bbb554b985aa691285a32e
lee <lee-dasilva@hotmail.com>
2020-04-01 14:19:38 +0100

    adding one more case to day-of-the-year

--- exercise/day-of-the-year.js
+++ exercise/day-of-the-year.js
@@ -17,5 +17,6 @@ t(() => dayOfTheYear(new Date('1664-08-09')) === 222)
 t(() => dayOfTheYear(new Date('1600-12-31')) === 366)
 t(() => dayOfTheYear(new Date('2020-06-22')) === 174)
 t(() => dayOfTheYear(new Date('2048-12-08')) === 343)
+t(() => dayOfTheYear(new Date('2048-11-08')) === 313)
 
 Object.freeze(tests)
