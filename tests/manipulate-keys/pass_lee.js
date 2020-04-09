332997e46562a088ebd67b9a561a0f88c3bad93a
lee <lee-dasilva@hotmail.com>
2020-04-09 16:12:55 +0100

    correcting solution

--- tests/manipulate-keys/pass_lee.js
+++ tests/manipulate-keys/pass_lee.js
@@ -1,31 +1,18 @@
-// filter
-const filterObj = (obj, filtering, keyVal) => {
-  let entrie = Object.entries(obj).filter(
-    (ele) => filtering(ele[keyVal]) && ele
-  )
+const filterKeys = (obj, filtering) => {
+  let entrie = Object.entries(obj).filter((ele) => filtering(ele[0]) && ele)
   let result = Object.fromEntries(entrie)
   return result
 }
 
-const filterKeys = (obj, filtering) => filterObj(obj, filtering, 0)
-
-// map
-const mapObj = (obj, f, keyVal) => {
+const mapKeys = (obj, f) => {
   let entrie = Object.entries(obj).map((ele) => {
-    if (!keyVal) {
-      ele = f(ele)
-      return ele
-    }
-    ele[keyVal - 1] = f(ele[keyVal - 1])
+    ele[0] = f(ele[0])
     return ele
   })
   let result = Object.fromEntries(entrie)
   return result
 }
 
-const mapKeys = (obj, f) => mapObj(obj, f, 1)
-
-// reduce
 const reduceKey = (obj, f, init) =>
   init == undefined
     ? Object.keys(obj).reduce(f)
--- tests/manipulate-values/pass_lee.js
+++ tests/manipulate-values/pass_lee.js
@@ -1,27 +1,16 @@
-const filterObj = (obj, filtering, keyVal) => {
-  let entrie = Object.entries(obj).filter(
-    (ele) => filtering(ele[keyVal]) && ele
-  )
+const filterValues = (obj, filtering) => {
+  let entrie = Object.entries(obj).filter((ele) => filtering(ele[1]) && ele)
   let result = Object.fromEntries(entrie)
   return result
 }
-
-const filterValues = (obj, filtering) => filterObj(obj, filtering, 1)
-
-const mapObj = (obj, f, keyVal) => {
+const mapValues = (obj, f) => {
   let entrie = Object.entries(obj).map((ele) => {
-    if (!keyVal) {
-      ele = f(ele)
-      return ele
-    }
-    ele[keyVal - 1] = f(ele[keyVal - 1])
+    ele[1] = f(ele[1])
     return ele
   })
   let result = Object.fromEntries(entrie)
   return result
 }
-
-const mapValues = (obj, f) => mapObj(obj, f, 2)
 
 const reduceValues = (obj, f, init) =>
   init == undefined
