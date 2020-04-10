2875d6ab4d4e0dbdb5a341302918850bb1391a9b
Christopher Fremond <christopher.fremond@gmail.com>
2020-04-10 13:40:33 +0100

    test for do-your-own-map

--- module/call-me-maybe.js
+++ module/call-me-maybe.js
@@ -1,6 +1,7 @@
 export const next = 'object'
 export const exercises = {
   map: { feedback: true }, // each / map
+  'do-your-own-map': { feedback: true }
   // filter / reject / partition
   // fold / foldr / reduce
   every: { feedback: true }, // some
