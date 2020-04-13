3a6d8542ba52fb0b772ec3e1c934980c365fa970
Christopher Fremond <christopher.fremond@gmail.com>
2020-04-13 12:20:07 +0100

    frenchris sol for every

---
+++ tests/every/pass_frenchris.js
@@ -0,0 +1,4 @@
+let len1 = arr1.every(el => el.length >= 5)
+let len2 = arr2.every(el => el.length >= 5)
+let lenRand1 = arrRand1.every(el => el.length >= 5)
+let lenRand2 = arrRand2.every(el => el.length >= 5)
