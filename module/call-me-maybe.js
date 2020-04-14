71edaf4f13f4a0ad3cc84bebcd644890b8d727f1
Christopher Fremond <christopher.fremond@gmail.com>
2020-04-14 16:12:58 +0100

    module call-me-maybe updated

--- module/call-me-maybe.js
+++ module/call-me-maybe.js
@@ -1,10 +1,13 @@
 export const next = 'object'
 export const exercises = {
-  map: { feedback: true }, // each / map
+  map: { feedback: true }, //add map ex based on box https://egghead.io/courses/professor-frisby-introduces-composable-functional-javascript
   'do-your-own-map': { feedback: true },
+  'for-each': { feedback: true },
+  'do-your-own-for-each': { feedback: true },
   // filter / reject / partition
   // fold / foldr / reduce
-  every: { feedback: true } // some
+  every: { feedback: true }, // some
+  'do-your-own-every': { feedback: true },
   // chunk
   // curry
 }
