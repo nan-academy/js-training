c261caea3aecedecfed38f749d37d5fe169606ba
Christopher Fremond <christopher.fremond@gmail.com>
2020-04-14 16:20:34 +0100

    frenchris solution : for each usage exercise

---
+++ tests/for-each/pass_frenchris.js
@@ -0,0 +1,7 @@
+arr1.forEach(el =>
+  el * 2 - 4 >= 0 ? arr2.push(el * 2 - 4) : arr2.push((el = null))
+)
+
+arrRandom1.forEach(el =>
+  el * 2 - 4 >= 0 ? arrRandom2.push(el * 2 - 4) : arrRandom2.push((el = null))
+)
