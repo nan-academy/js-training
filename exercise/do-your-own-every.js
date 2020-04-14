25cdaa8ebc0ece6db103a75023dc62f850ed1f96
Christopher Fremond <christopher.fremond@gmail.com>
2020-04-14 16:12:12 +0100

    Do your own every subject

--- exercise/do-your-own-every.js
+++ exercise/do-your-own-every.js
@@ -1,11 +1,13 @@
 /*
-## Do Your Own Every
+## Do-Your-Own-Every
 
 ### Instruction
 
-Create a function that works just like the `every` function that receives an
-array and a function, that tests whether all elements in the array passes the
+Create a function `Every` that works just like the [].every method that receives an
+array and a function, which tests whether or not all elements in the array passes the
 test implemented by the provided function.
+
+The use of `every` is forbidden for this exercise.
 
 ### Notions
 
@@ -17,26 +19,26 @@ test implemented by the provided function.
 ///*/// ⚡
 
 export const tests = []
-const t = (f) => tests.push(f)
+const t = f => tests.push(f)
 
 let array1 = [2, 24, 10, 32, 44, 8]
 let array2 = [18, 25, 33, 12, 55, 31]
 let array3 = []
 
 for (let i = 0; i < 7; i++) {
-  array3.push(Math.floor(Math.random() * 30) + 1)
+  array3.push(Math.floor(Math.random() * (40 - 0 + 1)))
 }
 
-const bigger10 = (n) => n > 10
-const div2 = (n) => n % 2 === 0
+const greaterThan10 = n => n > 10
+const lowerThan10 = n => n < 10
 
-t(() => array1.every(bigger10) === every(array1, bigger10))
-t(() => array2.every(bigger10) === every(array1, bigger10))
-t(() => array3.every(bigger10) === every(array1, bigger10))
+t(() => array1.every(greaterThan10) === Every(array1, greaterThan10))
+t(() => array2.every(greaterThan10) === Every(array2, greaterThan10))
+t(() => array3.every(greaterThan10) === Every(array3, greaterThan10))
 
-t(() => array1.every(div2) === every(array1, div2))
-t(() => array2.every(div2) === every(array1, div2))
-t(() => array3.every(div2) === every(array1, div2))
+t(() => array1.every(lowerThan10) === Every(array1, lowerThan10))
+t(() => array2.every(lowerThan10) === Every(array2, lowerThan10))
+t(() => array3.every(lowerThan10) === Every(array3, lowerThan10))
 
 t(({ code }) => !code.includes('every'))
 
