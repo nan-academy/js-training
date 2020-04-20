f99b392e7753ee7d2baec33eac3f28bfaf9b3291
Christopher Fremond <christopher.fremond@gmail.com>
2020-04-20 18:44:25 +0100

    Updated solution for frenchris for do your own filter

--- tests/do-your-own-filter/pass_frenchris.js
+++ tests/do-your-own-filter/pass_frenchris.js
@@ -3,3 +3,11 @@ const Filter = (el, fun) => {
   for (let e of el) if (fun(e)) answer.push(e)
   return answer
 }
+
+const Reject = (el, fun) => {
+  let answer = []
+  for (let e of el) if (!fun(e)) answer.push(e)
+  return answer
+}
+
+const Partition = (el, fun) => [Filter(el, fun), Reject(el, fun)]
