9f494ec9a1fc6ea1b7cd60dd7275491609b82943
MSilva95 <miguel-silva98@hotmail.com>
2020-04-22 13:49:35 +0100

    new exercise map-curry for objects

---
+++ tests/map-curry/pass_lee.js
@@ -0,0 +1,21 @@
+const mapEntriesCurry = (obj) => (fn) => {
+  return Object.fromEntries(Object.entries(obj).map(fn))
+}
+
+const addToCart = ([key, value]) => [key, value + 100]
+
+const reduceOBJ = (obj, f, init) => Object.entries(obj).reduce(f, init)
+
+const caloriesMedia = ([key, value]) => [
+  key,
+  reduceOBJ(
+    nutritionDB,
+    (acc, cr) => {
+      if (cr.includes(key)) {
+        acc['calories'] = (value * cr[1].calories) / 100
+      }
+      return acc
+    },
+    {}
+  ),
+]
