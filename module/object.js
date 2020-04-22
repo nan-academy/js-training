f664e58ab3e72b9f913aa360a3f96620692a61de
lee <lee-dasilva@hotmail.com>
2020-04-22 13:02:20 +0100

    object DNA/RNA

--- module/object.js
+++ module/object.js
@@ -1,5 +1,6 @@
 export const next = 'async'
 export const exercises = {
+  'molecules-cells': { feedback: true },
   fusion: { feedback: true },
   'manipulate-entries': { feedback: true },
   'deep-copy': { feedback: true },
