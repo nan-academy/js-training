66605b0505a5472cfddcca1b588bd81bed563151
lee <lee-dasilva@hotmail.com>
2020-04-23 13:20:11 +0100

    inverting test cases

--- exercise/manipulate-entries.js
+++ exercise/manipulate-entries.js
@@ -49,36 +49,43 @@ export const tests = []
 const t = (f) => tests.push(f)
 
 // filter entries
-t(({ eq }) => eq($filterE1, { garlic: 220, paprika: 480 }))
-t(({ eq }) => eq($filterE2, { oil: 500, onion: 230 }))
+t(({ eq }) =>
+  eq(
+    filterEntries(groceriesCart, ([k, v]) => k == 'garlic' || v == 480),
+    { garlic: 220, paprika: 480 }
+  )
+)
+t(({ eq }) =>
+  eq(
+    filterEntries(groceriesCart, ([k, v]) => /oil/.test(k) || v === 230),
+    { oil: 500, onion: 230 }
+  )
+)
 
 // map entries
-t(({ eq }) => eq($mapE1, $1))
-t(({ eq }) => eq($mapE2, { '✔️onion': 130 }))
+t(({ eq }) =>
+  eq(
+    mapEntries(groceriesCart, ([k, v]) => [`✔️${k}`, v - v]),
+    $mapE1
+  )
+)
+t(({ eq }) =>
+  eq(
+    mapEntries(
+      filterEntries(groceriesCart, ([k, v]) => k === 'onion'),
+      ([k, v]) => [`✔️${k}`, v - 100]
+    ),
+    { '✔️onion': 130 }
+  )
+)
 
 // reduce entries
-t(({ eq }) => eq(reduceEntries(groceriesCart), $2))
+t(({ eq }) => eq(reduceEntries(groceriesCart), $reduce))
 
 Object.freeze(tests)
 
-// filter entries
-const $filterE1 = filterEntries(
-  groceriesCart,
-  ([k, v]) => k == 'garlic' || v == 480
-)
-const $filterE2 = filterEntries(
-  groceriesCart,
-  ([k, v]) => /oil/.test(k) || v === 230
-)
-
 // map entries
-const $mapE1 = mapEntries(groceriesCart, ([k, v]) => [`✔️${k}`, v - v])
-const $mapE2 = mapEntries(
-  filterEntries(groceriesCart, ([k, v]) => k === 'onion'),
-  ([k, v]) => [`✔️${k}`, v - 100]
-)
-
-const $1 = {
+const $mapE1 = {
   '✔️oil': 0,
   '✔️onion': 0,
   '✔️garlic': 0,
@@ -86,7 +93,7 @@ const $1 = {
 }
 
 // prettier-ignore
-const $2 = {
+const $reduce = {
   oil: { calories: 240, protein: 0, carbs: 0, sugar: 615, fiber: 0, fat: 755},
   onion: { calories: 0, protein: 2.3, carbs: 20.7, sugar: 0, fiber: 0, fat: 0},
   garlic: { calories: 327.8, protein: 14.08, carbs: 72.6, sugar: 2.2, fiber: 4.62, fat: 1.1},
