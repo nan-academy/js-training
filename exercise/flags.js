f2190175845504112eefa1def88ac67a5542efd0
Clement Denis <le.mikmac@gmail.com>
2020-04-23 13:01:32 +0200

    object/flags: improve markdown subject

--- exercise/flags.js
+++ exercise/flags.js
@@ -3,35 +3,36 @@
 
 ### Instruction
 
-Create a function called `flags` that receives an object and outputs the specific flags from the
-properties of that object.
+Create a function called `flags` that receives an object and outputs
+the specific aliases and descriptions from the properties of that object.
 
-The `help` flag :
+The `help` flag:
   - Should be present in the output by default.
-  - When not present in the input, it should output the description of all flags. But when present it can specify the 
-  flags that you want to see the description of, ex: `help: ['divide']`
+  - When not present in the input, it should output the description of all flags.
+    But when present it can specify the flags that you want to see
+    the description of. (ex: `help: ['divide']`)
 
-Example :
+#### Example:
 
+```js
 {
   multiply: 'multiply the values',
   divide: 'divides the values',
   help: ['divide']
 }
-
+```
 and outputs :
 
+```js
 {
-  flags: { h: 'help', m: 'multiply', d: 'divide'}
-  help_description: {
-    divide: 'divides the values'
-  }
+  alias: { h: 'help', m: 'multiply', d: 'divide'}
+  description: '-d, --divide: divides the values',
 }
+```
 
 // /*/ // ⚡
 
 // /*/ // ⚡
-
 export const tests = []
 const t = (f) => tests.push(f)
 
