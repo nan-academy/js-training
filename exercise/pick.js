40199fc2d7e53ecafaa260ca2030dd10112c2603
Clement Denis <le.mikmac@gmail.com>
2020-04-27 21:21:45 +0200

    object/pick: include should fail, rewrite using setup + add failing case

--- exercise/pick.js
+++ exercise/pick.js
@@ -16,21 +16,24 @@ This function will pick only the keys, in the second argument, from the object p
 export const tests = []
 const t = (f) => tests.push(f)
 
-t(({ eq }) => eq(pick($person, ['firstName', 'lastName']), $newperson))
-t(({ eq }) => eq(pick($1, ['brand', 'year']), $new1))
-t(({ eq }) => eq(pick($data, 'age'), $newdata))
-t(({ eq }) => eq(pick($test, 'graphic'), {}))
+t(({ eq, ctx }) => eq(pick(ctx.agent, ['firstName', 'lastName']), ctx.newAgent))
+t(({ eq, ctx }) => eq(pick(ctx.car, ['brand', 'year']), ctx.newCar))
+t(({ eq, ctx }) => eq(pick(ctx.user, 'ageVerified'), ctx.newUser))
+t(({ eq, ctx }) => eq(pick(ctx.computer, 'graphic'), {}))
 
 Object.freeze(tests)
-const $person = {
-  firstName: 'James',
-  lastName: 'Bond',
-  age: 25,
-  email: 'jamesbond@hotmail.com',
-}
-const $newperson = { firstName: 'James', lastName: 'Bond' }
-const $1 = { brand: 'ford', motor: 'v8', year: 2000 }
-const $new1 = { brand: 'ford', year: 2000 }
-const $data = { firstName: 'John', lastName: 'Doe', age: 32 }
-const $newdata = { age: 32 }
-const $test = { brand: 'lenovo', ram: '32GB', processor: 'i7 8th Gen' }
+
+export const setup = () => ({
+  agent: {
+    firstName: 'James',
+    lastName: 'Bond',
+    age: 25,
+    email: 'jamesbond@hotmail.com',
+  },
+  newAgent: { firstName: 'James', lastName: 'Bond' },
+  car: { brand: 'ford', motor: 'v8', year: 2000 },
+  newCar: { brand: 'ford', year: 2000 },
+  user: { firstName: 'John', lastName: 'Doe', age: 32, ageVerified: false },
+  newUser: { ageVerified: false },
+  computer: { brand: 'lenovo', ram: '32GB', processor: 'i7 8th Gen' },
+})
