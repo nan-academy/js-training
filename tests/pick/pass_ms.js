410a3c52325c4ebd55795425c4ad4f8c4f2ff322
MSilva95 <miguel-silva98@hotmail.com>
2020-04-29 10:51:34 +0100

    adding omit to the exercise and changing sol

--- tests/pick/pass_ms.js
+++ tests/pick/pass_ms.js
@@ -1,5 +1,25 @@
-const pick = (o, keys) => {
+function pick(obj, keys) {
   const result = {}
-  for (let [k, v] of Object.entries(o)) keys.includes(k) ? (result[k] = v) : {}
-  return result
+  if (Array.isArray(keys)) {
+    for (let [k, v] of Object.entries(obj))
+      keys.indexOf(k) != -1 ? (result[k] = v) : {}
+    return result
+  }
+  return Object.fromEntries(
+    Object.entries(obj).filter(([key]) => [keys].indexOf(key) != -1)
+  )
+}
+
+function omit(obj, keys) {
+  var target = {}
+  for (let i in obj) {
+    if (keys.indexOf(i) != -1) {
+      continue
+    }
+    if (!Object.prototype.hasOwnProperty.call(obj, i)) {
+      continue
+    }
+    target[i] = obj[i]
+  }
+  return target
 }
