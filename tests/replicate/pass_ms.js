7a2f261bc7bd9ef0ecb60c9ebfd28fc1ab3b6a56
MSilva95 <miguel-silva98@hotmail.com>
2020-04-29 18:02:11 +0100

    fix indent

--- tests/replicate/pass_ms.js
+++ tests/replicate/pass_ms.js
@@ -8,10 +8,10 @@ function copyObject(target, ...objs) {
   for (let source of objs) {
     for (let [k] of Object.entries(source)) {
       let vs = source[k]
-      if(isObject(vs) && isObject(target[k])){
-        target[k] = copyObject(target[k], vs) 
-      } else{
-          target[k] = vs
+      if (isObject(vs) && isObject(target[k])) {
+        target[k] = copyObject(target[k], vs)
+      } else {
+        target[k] = vs
       }
       target[k] = target[k]
     }
