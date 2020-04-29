5ec856a7f88158bcf05246f6a42f31fdc513a1e3
lee <lee-dasilva@hotmail.com>
2020-04-29 15:34:41 +0100

    adding fixed solutions

--- tests/manipulate-entries/pass_lee.js
+++ tests/manipulate-entries/pass_lee.js
@@ -3,20 +3,24 @@ const filterEntries = (obj, f) =>
 
 const mapEntries = (obj, f) => Object.fromEntries(Object.entries(obj).map(f))
 
-const reduceOBJ = (obj, f, init) =>
-  init == undefined
-    ? Object.entries(obj).reduce(f)
-    : Object.entries(obj).reduce(f, init)
+const reduceEntries = (obj, ...rest) => Object.entries(obj).reduce(...rest)
 
-const reduceEntries = (obj) =>
-  reduceOBJ(
-    obj,
-    (acc, cr) => {
-      acc[cr[0]] = mapEntries(nutritionDB[cr[0]], ([k, v]) => [
-        k,
-        (v * cr[1]) / 100,
-      ])
-      return acc
-    },
-    {}
+const totalCalories = (obj) =>
+  Number(
+    reduceEntries(
+      obj,
+      (acc, cr) => (nutritionDB[cr[0]].calories * cr[1]) / 100 + acc,
+      0
+    ).toFixed(1)
   )
+
+const lowCarbs = (obj) =>
+  filterEntries(obj, ([k, v]) => (nutritionDB[k].carbs * v) / 100 < 50)
+
+const media = (cartAmount, dbAmount) =>
+  Number(((cartAmount * dbAmount) / 100).toFixed(3))
+const cartTotal = (obj) =>
+  mapEntries(obj, ([k, v]) => [
+    k,
+    mapEntries(nutritionDB[k], ([dbK, dbV]) => [dbK, media(v, dbV)]),
+  ])
