94b4ae764d42574a7b30add6cc995a97a2ddee30
MSilva95 <miguel-silva98@hotmail.com>
2020-04-29 17:38:24 +0100

    adding ms sol

---
+++ tests/replicate/pass_ms.js
@@ -0,0 +1,20 @@
+const replica = (target, ...obj) => copyObject(target, ...obj)
+
+const isObject = (item) => {
+  return item && typeof item === 'object' && !Array.isArray(item)
+}
+
+function copyObject(target, ...objs) {
+  for (let source of objs) {
+    for (let [k] of Object.entries(source)) {
+      let vs = source[k]
+      if(isObject(vs) && isObject(target[k])){
+        target[k] = copyObject(target[k], vs) 
+      } else{
+          target[k] = vs
+      }
+      target[k] = target[k]
+    }
+  }
+  return target
+}
