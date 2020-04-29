965cc0dc00e526265238f084fa702ba6928bf4c0
MSilva95 <miguel-silva98@hotmail.com>
2020-04-29 17:53:07 +0100

    adding ms sol

---
+++ tests/deep-copy/pass_ms.js
@@ -0,0 +1,14 @@
+function cloneDeep(target, ...objs) {
+  for (let source of objs) {
+    for (let [k] of Object.entries(source)) {
+      let vs = source[k]
+      if (isObject(vs) && isObject(target[k])) {
+        target[k] = copyObject(target[k], vs)
+      } else {
+        target[k] = vs
+      }
+      target[k] = target[k]
+    }
+  }
+  return target
+}
