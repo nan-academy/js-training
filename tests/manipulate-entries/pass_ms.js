630cca20a441fbea95b150addfb8503eb128a904
MSilva95 <miguel-silva98@hotmail.com>
2020-04-30 10:34:34 +0100

    ms sol

--- tests/manipulate-entries/pass_ms.js
+++ tests/manipulate-entries/pass_ms.js
@@ -1,23 +1,39 @@
-const filterEntries = (obj, f) =>
-  Object.fromEntries(Object.entries(obj).filter(f))
-
-const mapEntries = (obj, f) => Object.fromEntries(Object.entries(obj).map(f))
-
-const red = (obj, f, i) => {
-  if (i == undefined) {
-    return Object.entries(obj).reduce(f)
-  } else {
-    return Object.entries(obj).reduce(f, i)
-  }
+const filterEntries = (obj, f) => {
+  return Object.fromEntries(Object.entries(obj).filter(f))
 }
 
-const reduceEntries = (obj) =>
-  red(obj,(acc, cr) => {
-      acc[cr[0]] = mapEntries(nutritionDB[cr[0]], ([k, v]) => [
-        k,
-        (v * cr[1]) / 100,
-      ])
-      return acc
-    },
-    {}
+const mapEntries = (obj, f) => {
+  return Object.fromEntries(Object.entries(obj).map(f))
+}
+
+const reduceEntries = (obj, ...k) => {
+  return Object.entries(obj).reduce(...k)
+}
+
+const floats = (n) => {
+  return Number(n.toFixed(10))
+}
+
+const lowCarbs = (car) => {
+  return filterEntries(car, ([k, v]) => (nutritionDB[k].carbs * v) / 100 < 50)
+}
+
+const totalCalories = (car) => {
+  return floats(
+    reduceEntries(
+      car,
+      (acc, cr) => (nutritionDB[cr[0]].calories * cr[1]) / 100 + acc,
+      0
+    )
   )
+}
+
+const cartTotal = (car) => {
+  return mapEntries(car, ([k, v]) => [
+    k,
+    mapEntries(nutritionDB[k], ([ing, howmany]) => [
+      ing,
+      floats((v / 100) * howmany),
+    ]),
+  ])
+}
