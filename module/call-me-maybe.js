8dd2cef41ab3a25dfafc3c78ff0471a010402a48
Christopher Fremond <christopher.fremond@gmail.com>
2020-05-04 16:55:42 +0100

    module updated for "do your own filter" exercise to run

--- module/call-me-maybe.js
+++ module/call-me-maybe.js
@@ -3,5 +3,6 @@ export const exercises = {
   'using-map': { feedback: true },
   filter: { feedback: true },
   map: { feedback: true },
-   'for-each': { feedback: true },
+  'for-each': { feedback: true },
+  'do-your-own-filter': { feeback: true },
 }
