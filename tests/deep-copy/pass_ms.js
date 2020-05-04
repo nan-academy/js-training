eb262f33e79198901cf0dcdca976166e881db70f
MSilva95 <miguel-silva98@hotmail.com>
2020-05-04 16:55:24 +0100

    ms sol

--- tests/deep-copy/pass_ms.js
+++ tests/deep-copy/pass_ms.js
@@ -1,14 +1,12 @@
-function deepCopy(target, ...objs) {
-  for (let source of objs) {
-    for (let [k] of Object.entries(source)) {
-      let vs = source[k]
-      if (isObject(vs) && isObject(target[k])) {
-        target[k] = copyObject(target[k], vs)
-      } else {
-        target[k] = vs
-      }
-      target[k] = target[k]
+function deepCopy(obj) {
+  let res = Array.isArray(obj) ? [] : obj.constructor === Object ? {} : obj
+  for (let k of Object.keys(obj)) {
+    let v = obj[k]
+    if (v && typeof v === 'object' && v !== null) {
+      res[k] = deepCopy(v)
+    } else {
+      res[k] = v
     }
   }
-  return target
+  return res
 }
