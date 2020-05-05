9888463f3f8088d7750904c33d18af3973e2ebd5
lee <lee-dasilva@hotmail.com>
2020-05-05 18:13:06 +0100

    replacing null to undefined

--- tests/deep-copy/pass_lee.js
+++ tests/deep-copy/pass_lee.js
@@ -2,7 +2,7 @@ const isObj = (x) => x && (x.constructor === Object || !x.constructor)
 const deepCopy = (target) => {
   let result = Array.isArray(target) ? [] : isObj(target) ? {} : target
   for (let [k, v] of Object.entries(target)) {
-    result[k] = typeof v === 'object' && v !== null ? deepCopy(v) : v
+    result[k] = typeof v === 'object' && v !== undefined ? deepCopy(v) : v
   }
   return result
 }
