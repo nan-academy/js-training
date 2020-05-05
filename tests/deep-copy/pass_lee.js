fb27dbe10ee9254ceaeff07debfbd9553e23248f
lee <lee-dasilva@hotmail.com>
2020-05-05 14:31:01 +0100

    adding test case and solution correction

--- tests/deep-copy/pass_lee.js
+++ tests/deep-copy/pass_lee.js
@@ -1,6 +1,6 @@
 const isObj = (x) => x && (x.constructor === Object || !x.constructor)
 const deepCopy = (target) => {
-  var result = Array.isArray(target) ? [] : isObj(target) ? {} : target
+  let result = Array.isArray(target) ? [] : isObj(target) ? {} : target
   for (let [k, v] of Object.entries(target)) {
     result[k] = typeof v === 'object' && v !== null ? deepCopy(v) : v
   }
