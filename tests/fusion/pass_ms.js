463f0d6a3e9810badf9318d87afed54724297824
MSilva95 <miguel-silva98@hotmail.com>
2020-05-05 13:14:00 +0100

    fixing the solution

--- tests/fusion/pass_ms.js
+++ tests/fusion/pass_ms.js
@@ -3,13 +3,16 @@ const fusion = (obj, res) => {
   for (let k of Object.keys(res)) {
     if (!obj[k] || !res[k]) {
       newObj[k] = res[k]
-    } else if (typeof obj[k] === 'number') {
+    } else if (typeof obj[k] === 'number' && typeof res[k] === 'number') {
       newObj[k] = obj[k] + res[k]
-    } else if (typeof obj[k] === 'string') {
+    } else if (typeof obj[k] === 'string' && typeof res[k] === 'string') {
       newObj[k] = obj[k].concat(' ', res[k])
-    } else if (obj[k].constructor === Object || !obj[k].constructor) {
+    } else if (
+      (obj[k].constructor === Object || !obj[k].constructor) &&
+      (res[k].constructor === Object || !res[k].constructor)
+    ) {
       newObj[k] = fusion(obj[k], res[k])
-    } else if (Array.isArray(obj[k])) {
+    } else if (Array.isArray(obj[k]) && Array.isArray(res[k])) {
       newObj[k] = obj[k].concat(res[k])
     } else {
       Object.assign(newObj, res)
@@ -17,5 +20,3 @@ const fusion = (obj, res) => {
   }
   return newObj
 }
-
-const isObj = (obj) => obj.constructor === Object || !obj.constructor
