68b35bf131e95cb71f221ef5def3eff682d8ed56
OGordoo <marcosafonso8@hotmail.com>
2020-05-05 14:33:50 +0100

    Afonso Bloody Sunday solution fix

--- tests/bloody-sunday/pass_afonso.js
+++ tests/bloody-sunday/pass_afonso.js
@@ -1,33 +1,48 @@
-const bloodySunday = (date) => {
-  let year = date.getUTCFullYear()
-  let month = date.getUTCMonth()
-  let day = date.getUTCDate()
-  let d = new Date()
-  d.setFullYear(year, month, day)
+const days = [
+  'Saturday',
+  'Monday',
+  'Tuesday',
+  'Wednesday',
+  'Thursday',
+  'Friday'
+]
 
-  let s = new Date()
-  s.setFullYear(1, 0, 1)
+const isLeapYear = (year) =>
+  year % 4 === 0 && (year % 100 !== 0 || year % 400 === 0)
 
-  let nbr = 1
-  while (s.getTime() !== d.getTime()) {
-    s.setDate(s.getDate() + 1)
-    nbr++
-    if (nbr === 7) {
-      nbr = 1
+const leapYears = (date) => {
+  const year = date.getFullYear()
+  let count = -1
+  for (let i = 1; i < year; i++) {
+    count = count + (isLeapYear(i) ? 1 : 0)
+  }
+  return count
+}
+
+const dayOfYear = (date) => {
+  const month = date.getUTCMonth()
+  let total = date.getUTCDate(),
+    i = 0
+  while (i < month) {
+    if (i === 1) {
+      total += isLeapYear(date.getFullYear()) ? 29 : 28
+    } else {
+      total += i <= 6 ? (i % 2 === 0 ? 31 : 30) : i % 2 === 0 ? 30 : 31
     }
+    i++
   }
-  switch (nbr) {
-    case 1:
-      return 'Monday'
-    case 2:
-      return 'Tuesday'
-    case 3:
-      return 'Wednesday'
-    case 4:
-      return 'Thursday'
-    case 5:
-      return 'Friday'
-    case 6:
-      return 'Saturday'
-  }
+  return total
+}
+
+const bloodySunday = (date) => {
+  const year = date.getFullYear()
+  return year === 0
+    ? days[(leapYears(date) + dayOfYear(date)+1) % 6]
+    : days[
+    ((date.getUTCFullYear() - 1) * 365 +
+      leapYears(date) +
+      dayOfYear(date) +
+      1) %
+    6
+      ]
 }
