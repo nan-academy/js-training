49d9e185e80d77069b26a14c04b7c95a79003555
Clement Denis <le.mikmac@gmail.com>
2020-05-06 18:12:45 +0200

    call-me-maybe/filter: fix subject and rework tests

--- tests/filter/pass_afonso.js
+++
@@ -1,4 +0,0 @@
-let arr2 = arr1.filter((x) => x % 2 === 0)
-let arr3 = arr1.filter((x) => x % 3 === 0)
-let arr4 = arr1.filter((x) => (x % 3 === 0) & (x % 2 === 0))
-let arrRandom2 = arr1.filter((x) => (x % 3 === 0) & (x % 2 === 0))
