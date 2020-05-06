e625b0143cf1556a196e830206ace264837f08f1
MSilva95 <miguel-silva98@hotmail.com>
2020-05-06 11:48:18 +0100

    fixing sol

--- tests/deep-copy/pass_ms.js
+++ tests/deep-copy/pass_ms.js
@@ -2,7 +2,7 @@ function deepCopy(obj) {
   let res = Array.isArray(obj) ? [] : obj.constructor === Object ? {} : obj
   for (let k of Object.keys(obj)) {
     let v = obj[k]
-    if (v && typeof v === 'object' && v !== null) {
+    if ((v && typeof v === 'object' && v !== null) || Array.isArray(v)) {
       res[k] = deepCopy(v)
     } else {
       res[k] = v
