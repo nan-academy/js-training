29cdfc38b4a12c2c0180924a4727f6da0c4fbb89
OGordoo <marcosafonso8@hotmail.com>
2020-05-06 18:39:10 +0100

    changing exercise name

--- exercise/using-reduce.js
+++ exercise/using-reduce.js
@@ -1,12 +1,14 @@
 /*
-## Reduce
+## Using Reduce
 
 ### Instruction
 
 Create three functions :
 - `adder` that receives an array and adds its elements.
+
 - `sumOrMul` that receives an array and adds or multiplies its elements
 depending on whether the element is an odd or an even number.
+
 - `funcExec` that receives an array of functions and executes them.
 
 All functions may or may not receive an extra argument that should be the
--- module/call-me-maybe.js
+++ module/call-me-maybe.js
@@ -2,9 +2,9 @@ export const next = 'object'
 export const exercises = {
   'using-map': { feedback: true },
   'using-filter': { feedback: true },
+  'using-reduce': { feedback: true },
   'for-each': { feedback: true },
   filter: { feedback: true },
-  reduce: { feedback: true },
   'sweet-curry': { feedback: true },
   'has-city': { feedback: true },
   'currify': { feedback: true },
