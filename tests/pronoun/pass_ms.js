29cb705d60a45efae338137730eeeadca7157a02
MSilva95 <miguel-silva98@hotmail.com>
2020-05-06 14:09:40 +0100

    sol ms

--- tests/pronoun/pass_ms.js
+++ tests/pronoun/pass_ms.js
@@ -1,38 +1,32 @@
 const pronoun = (str) => {
   let a = {}
   let c = { word: [], count: 0 }
-  let b = str.toLowerCase().split(' ')
+  let b = str.toLowerCase().split(/[^\w]/g)
+  let reg = /^(i|you|he|she|it|we|you|they)$/
 
   for (let ele of b) {
-    if (/^(i|you|he|she|it|we|you|they)$/.test(ele)) {
-      a[ele] = copyObject(c)
+    if (reg.test(ele)) {
+      a[ele] = deepCopy(c)
     }
   }
   for (let i = 0; i < b.length; i++) {
     if (a[b[i]]) {
-      if (i != b.length - 1) a[b[i]].word.push(b[i + 1].replace(/\n/, ''))
+      if (b[i + 1] && !reg.test(b[i + 1])) a[b[i]].word.push(b[i + 1])
       a[b[i]].count++
     }
   }
   return a
 }
 
-const copyObject = (target) => {
-  if (!Array.isArray(target)) {
-    let res = {}
-    for (let [key, value] of Object.entries(target)) {
-      res[key] =
-        typeof value === 'object' && value !== null ? copyObject(value) : value
+function deepCopy(obj) {
+  let res = Array.isArray(obj) ? [] : obj.constructor === Object ? {} : obj
+  for (let k of Object.keys(obj)) {
+    let v = obj[k]
+    if ((v && typeof v === 'object' && v !== null) || Array.isArray(v)) {
+      res[k] = deepCopy(v)
+    } else {
+      res[k] = v
     }
-    return res
-  } else if (Array.isArray(target)) {
-    let ans = []
-    for (let [key, value] of Object.entries(target)) {
-      ans[key] =
-        typeof value === Array.isArray && value !== null
-          ? copyObject(value)
-          : value
-    }
-    return ans
   }
+  return res
 }
