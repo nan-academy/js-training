b1dbb35e892a8aa7ac9e4bd878fd14d73d21eadc
lee <lee-dasilva@hotmail.com>
2020-05-06 11:18:25 +0100

    adding to module

--- exercise/manipulate-values.js
+++ exercise/manipulate-values.js
@@ -37,108 +37,83 @@ const nutritionDB = {
     sugar:   { calories: 387, protein: 0,     carbs: 100,   sugar: 100, fiber: 0,   fat: 0     },
     orange:  { calories: 49,  protein: 0.9,   carbs: 13,    sugar: 9,   fiber: 0.2, fat: 0.1   },
   }
-  
-  // /*/ // ⚡
-  
-  // /*/ // ⚡
-  
-  export const tests = []
-  const t = (f) => tests.push(f)
-  
-  // filter values
-  t(({ ctx, eq }) =>
-    eq(
-      filterValues(ctx.groceriesCart, (v) => v < 80),
-      ctx.filterLess
-    )
+
+// /*/ // ⚡
+
+// /*/ // ⚡
+
+export const tests = []
+const t = (f) => tests.push(f)
+
+// filter values
+t(({ ctx, eq }) =>
+  eq(
+    filterValues(ctx.groceriesCart, (v) => v < 80),
+    { oil: 50, garlic: 22 }
   )
-  t(({ ctx, eq }) =>
-    eq(
-      filterValues(ctx.groceriesCart, (v) => v === 220),
-      ctx.equalTo
-    )
+)
+t(({ ctx, eq }) =>
+  eq(
+    filterValues(
+      nutritionDB,
+      (v) => Object.entries(filterValues(v, (ele) => ele === 0)).length !== 0
+    ),
+    ctx.have0
   )
-  t(({ ctx, eq }) =>
-    eq(
-      filterValues(
-        nutritionDB,
-        (v) => Object.entries(filterValues(v, (ele) => ele === 0)).length !== 0
-      ),
-      ctx.have0
-    )
+)
+
+// map value
+t(({ ctx, eq }) =>
+  eq(
+    mapValues(
+      filterValues(ctx.groceriesCart, (v) => v >= 200),
+      (ele) => ele - 100
+    ),
+    { tomato: 100, onion: 120 }
   )
-  
-  // map value
-  t(({ ctx, eq }) =>
-    eq(
-      mapValues(
-        filterValues(ctx.groceriesCart, (v) => v >= 200),
-        (ele) => ele - 100
-      ),
-      ctx.minus
-    )
-  )
-  t(({ ctx, eq }) =>
-    eq(
-      mapValues(ctx.groceriesCart, (ele) => ele + 100),
-      ctx.plus
-    )
-  )
-  
-  // reduce value
-  t(({ ctx }) => reduceValues(ctx.groceriesCart, (acc, cr) => acc + cr) === 572)
-  t(() => reduceValues({ a: 1, b: 2, c: 3 }, (acc, cr) => acc + cr) === 6)
-  t(() => reduceValues({ a: 1, b: 2, c: 3 }, (acc, cr) => acc + cr, 3) === 9)
-  
-  Object.freeze(tests)
-  
-  export const setup = () => ({
-    groceriesCart: {
-      tomato: 200,
-      vinegar: 80,
-      oil: 50,
-      onion: 220,
-      garlic: 22,
+)
+
+// reduce value
+t(({ ctx }) => reduceValues(ctx.groceriesCart, (acc, cr) => acc + cr) === 572)
+t(() => reduceValues({ a: 1, b: 2, c: 3 }, (acc, cr) => acc + cr) === 6)
+t(() => reduceValues({ a: 1, b: 2, c: 3 }, (acc, cr) => acc + cr, 3) === 9)
+
+Object.freeze(tests)
+
+export const setup = () => ({
+  groceriesCart: {
+    tomato: 200,
+    vinegar: 80,
+    oil: 50,
+    onion: 220,
+    garlic: 22,
+  },
+  have0: {
+    vinegar: {
+      calories: 20,
+      protein: 0.04,
+      carbs: 0.6,
+      sugar: 0.4,
+      fiber: 0,
+      fat: 0,
     },
-    // filter values
-    filterLess: { oil: 50, garlic: 22 },
-    equalTo: { onion: 220 },
-    // map value
-    minus: { tomato: 100, onion: 120 },
-    plus: {
-      tomato: 300,
-      vinegar: 180,
-      oil: 150,
-      onion: 320,
-      garlic: 122,
+    oil: { calories: 48, protein: 0, carbs: 0, sugar: 123, fiber: 0, fat: 151 },
+    onion: { calories: 0, protein: 1, carbs: 9, sugar: 0, fiber: 0, fat: 0 },
+    paprika: {
+      calories: 282,
+      protein: 14.14,
+      carbs: 53.99,
+      sugar: 1,
+      fiber: 0,
+      fat: 12.89,
     },
-    have0: {
-      vinegar: {
-        calories: 20,
-        protein: 0.04,
-        carbs: 0.6,
-        sugar: 0.4,
-        fiber: 0,
-        fat: 0,
-      },
-      oil: { calories: 48, protein: 0, carbs: 0, sugar: 123, fiber: 0, fat: 151 },
-      onion: { calories: 0, protein: 1, carbs: 9, sugar: 0, fiber: 0, fat: 0 },
-      paprika: {
-        calories: 282,
-        protein: 14.14,
-        carbs: 53.99,
-        sugar: 1,
-        fiber: 0,
-        fat: 12.89,
-      },
-      sugar: {
-        calories: 387,
-        protein: 0,
-        carbs: 100,
-        sugar: 100,
-        fiber: 0,
-        fat: 0,
-      },
+    sugar: {
+      calories: 387,
+      protein: 0,
+      carbs: 100,
+      sugar: 100,
+      fiber: 0,
+      fat: 0,
     },
-  })
-  
+  },
+})
