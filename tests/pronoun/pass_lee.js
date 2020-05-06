a1c02c19d3a79c3c7bcfccb9911329c7b768c533
lee <lee-dasilva@hotmail.com>
2020-05-06 12:17:38 +0100

    optimise solution

--- tests/pronoun/pass_lee.js
+++ tests/pronoun/pass_lee.js
@@ -1,15 +1,15 @@
 const pronoun = (str) => {
   let b = str.toLowerCase().split(/[^\w]/g)
   let a = {}
+  let reg = /^(i|you|he|she|it|we|you|they)$/i
   for (let i = 0; i < b.length; i++) {
-    if (/^(i|you|he|she|it|we|you|they)$/.test(b[i])) {
+    if (reg.test(b[i])) {
       if (!a[b[i]]) {
         a[b[i]] = {}
         a[b[i]]['word'] = []
         a[b[i]]['count'] = 0
       }
-      if (b[i + 1] && !/^(i|you|he|she|it|we|you|they)$/.test(b[i+1]))
-      a[b[i]].word.push(b[i + 1])
+      if (b[i + 1] && !reg.test(b[i + 1])) a[b[i]].word.push(b[i + 1])
       a[b[i]].count++
     }
   }
