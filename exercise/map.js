85a8e731398445181975b906a1c10759b0d3c662
Clement Denis <le.mikmac@gmail.com>
2020-05-06 19:00:49 +0200

    call-me-maybe/map: rename and add failing test for for..in

--- exercise/map.js
+++ exercise/map.js
@@ -1,20 +1,18 @@
 /*
-## Do-Your-Own-Map
+## Map
 
 ### Instructions
 
-1) Create a `map` function that takes an array as first argument, a function as second,
+- Create a `map` function that takes an array as first argument, a function as second,
 and that works like the method .map
 
-2) Create a `flatMap` function that takes an array as first argument, a function as second,
+- Create a `flatMap` function that takes an array as first argument, a function as second,
 and that works like the method .flatMap
 
 ### Notions
 
-- Map
-https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/map
-
-- FlatMap
+- https://devdocs.io/javascript/global_objects/array/map
+- https://devdocs.io/javascript/global_objects/array/flatmap
 
 */
 Array.prototype.map = undefined
@@ -28,67 +26,42 @@ Array.prototype.flat = undefined
 export const tests = []
 const t = (f) => tests.push(f)
 
-export const setup = () => {
-  const arr1 = [10, -10, 20, -95, 86, 102, 35, 89, 110]
-  const arr2 = [[10], -10, 20, -95, 86, [102], [35, 89], 110]
-  const arr3 = [[5], [4], [-3], [20], [17], [-33], [-4], [18]]
-  const arr4 = [
-    'Colombia',
-    'Mexico',
-    'El Salvador',
-    'Spanish speaking countries',
-  ]
-  const arr5 = [
-    'Perou',
-    'Brazil',
-    'Argentina',
-    'Venezuela',
-    'countries in South America',
-  ]
-  const arr6 = ['France', 'Portugal', 'Italy', 'members of the EU']
-
-  Object.freeze(arr1)
-  Object.freeze(arr2)
-  Object.freeze(arr3)
-  Object.freeze(arr4)
-  Object.freeze(arr5)
-  Object.freeze(arr6)
-
-  return { arr1, arr2, arr3, arr4, arr5, arr6 }
-}
-
 const add1 = (el) => el + 1
-const mult2 = (el) => el * 2
 const sub3 = (el) => el - 3
+const mult2 = (el) => el * 2
 
 const doAll = (el) => sub3(mult2(add1(el)))
 const posValsIndex = (el, i) => (el >= 0 ? `${i}: ${el}` : undefined)
 const indexValsArray = (el, i, arr) =>
   `${el} is at index: ${i} out of ${arr.length - 1}`
-const arrayFormatSentence = (item, index, arr) => {
-  if (Number(index) === arr.length - 2) return `and ${arr[arr.length - 2]} `
-  if (Number(index) === arr.length - 1)
+
+const arrayFormatSentences = (item, index, arr) => {
+  if (index === arr.length - 2) return `and ${arr[arr.length - 2]} `
+  if (index === arr.length - 1) {
     return `are ${String(arr.length - 1)} ${item}.`
+  }
   return `${item}, `
 }
 
-// Map
-
+// map
 t(({ eq, ctx }) =>
-  eq(map(ctx.arr1, add1), [11, -9, 21, -94, 87, 103, 36, 90, 111])
-)
-t(({ eq, ctx }) =>
-  eq(map(ctx.arr1, mult2), [20, -20, 40, -190, 172, 204, 70, 178, 220])
-)
-t(({ eq, ctx }) =>
-  eq(map(ctx.arr1, sub3), [7, -13, 17, -98, 83, 99, 32, 86, 107])
-)
-t(({ eq, ctx }) =>
-  eq(map(ctx.arr1, doAll), [19, -21, 39, -191, 171, 203, 69, 177, 219])
+  eq(map(ctx.numbers, add1), [11, -9, 21, -94, 87, 103, 36, 90, 111])
 )
 
 t(({ eq, ctx }) =>
-  eq(map(ctx.arr1, posValsIndex), [
+  eq(map(ctx.numbers, mult2), [20, -20, 40, -190, 172, 204, 70, 178, 220])
+)
+
+t(({ eq, ctx }) =>
+  eq(map(ctx.numbers, sub3), [7, -13, 17, -98, 83, 99, 32, 86, 107])
+)
+
+t(({ eq, ctx }) =>
+  eq(map(ctx.numbers, doAll), [19, -21, 39, -191, 171, 203, 69, 177, 219])
+)
+
+t(({ eq, ctx }) =>
+  eq(map(ctx.numbers, posValsIndex), [
     '0: 10',
     undefined,
     '2: 20',
@@ -102,7 +75,7 @@ t(({ eq, ctx }) =>
 )
 
 t(({ eq, ctx }) =>
-  eq(map(ctx.arr1, indexValsArray), [
+  eq(map(ctx.numbers, indexValsArray), [
     '10 is at index: 0 out of 8',
     '-10 is at index: 1 out of 8',
     '20 is at index: 2 out of 8',
@@ -117,33 +90,32 @@ t(({ eq, ctx }) =>
 
 t(({ eq, ctx }) =>
   eq(
-    map(ctx.arr4, arrayFormatSentence).join(''),
+    map(ctx.sentences[0], arrayFormatSentence).join(''),
     'Colombia, Mexico, and El Salvador are 3 Spanish speaking countries.'
   )
 )
 
 t(({ eq, ctx }) =>
   eq(
-    map(ctx.arr5, arrayFormatSentence).join(''),
+    map(ctx.sentences[1], arrayFormatSentence).join(''),
     'Perou, Brazil, Argentina, and Venezuela are 4 countries in South America.'
   )
 )
 
 t(({ eq, ctx }) =>
   eq(
-    map(ctx.arr6, arrayFormatSentence).join(''),
+    map(ctx.sentences[2], arrayFormatSentence).join(''),
     'France, Portugal, and Italy are 3 members of the EU.'
   )
 )
 
-//flatMap
-
+// flatMap
 t(({ eq, ctx }) =>
-  eq(flatMap(ctx.arr2, add1), ['101', -9, 21, -94, 87, '1021', '35,891', 111])
+  eq(flatMap(ctx.mixed, add1), ['101', -9, 21, -94, 87, '1021', '35,891', 111])
 )
 
 t(({ eq, ctx }) =>
-  eq(flatMap(ctx.arr2, posValsIndex), [
+  eq(flatMap(ctx.mixed, posValsIndex), [
     '0: 10',
     undefined,
     '2: 20',
@@ -156,7 +128,7 @@ t(({ eq, ctx }) =>
 )
 
 t(({ eq, ctx }) =>
-  eq(flatMap(ctx.arr3, indexValsArray), [
+  eq(flatMap(ctx.nested, indexValsArray), [
     '5 is at index: 0 out of 7',
     '4 is at index: 1 out of 7',
     '-3 is at index: 2 out of 7',
@@ -169,3 +141,24 @@ t(({ eq, ctx }) =>
 )
 
 Object.freeze(tests)
+
+export const setup = () => {
+  const numbers = [10, -10, 20, -95, 86, 102, 35, 89, 110]
+  const mixed = [[10], -10, 20, -95, 86, [102], [35, 89], 110]
+  const nested = [[5], [4], [-3], [20], [17], [-33], [-4], [18]]
+  const sentences = [
+    ['Colombia', 'Mexico', 'El Salvador', 'Spanish speaking countries'],
+    ['Perou', 'Brazil', 'Argentina', 'Venezuela', 'countries in South America'],
+    ['France', 'Portugal', 'Italy', 'members of the EU'],
+  ]
+
+  Object.getPrototypeOf([]).proto = ' [avoid for..in] '
+  Object.freeze(numbers)
+  Object.freeze(mixed)
+  Object.freeze(nested)
+  Object.freeze(sentences[0])
+  Object.freeze(sentences[1])
+  Object.freeze(sentences[2])
+
+  return { numbers, mixed, nested, sentences }
+}
--- tests/do-your-own-map/fail_built-in.js
+++
@@ -1,1 +0,0 @@
-const MapFunc = (f => f.call.bind(f))([]['m'+'ap'])
