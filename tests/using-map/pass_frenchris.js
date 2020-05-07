8c20a51203e640c576da04691ab27d8861e132da
Christopher Fremond <christopher.fremond@gmail.com>
2020-05-07 18:02:50 +0100

    updated solution for 'using-map' (former map) for frenchris

--- tests/using-map/pass_frenchris.js
+++ tests/using-map/pass_frenchris.js
@@ -1,8 +1,34 @@
-const map = (el, fn) => {
-  let result = []
-  for (let i in el) result.push(fn(el[i], i, el))
-  return result
-}
+const citiesOnly = (el) => el.map((el) => el.city)
 
-const flat = (el) => (Array.isArray(el) ? [...el] : el)
-const flatMap = (el, fn) => flat(map(el, fn))
+const upperCasingStates = (el) =>
+  el
+    .map((el) => el.split(' '))
+    .map((arr) => arr.map((word) => `${word[0].toUpperCase()}${word.slice(1)}`))
+    .map((el) => el.join(' '))
+
+const farenheitToCelsius = (el) =>
+  el
+    .map((e) => Number(e.slice(0, -2)))
+    .map((e) => e - 32)
+    .map((e) => e * 5)
+    .map((e) => e / 9)
+    .map((e) => Math.floor(e))
+    .map((e) => `${String(e)}°C`)
+
+const trimTemp = (obj) =>
+  obj.map(({ temperature, ...el }) => ({
+    ...el,
+    temperature: temperature.split(' ').join(''),
+  }))
+
+const formatObject = (obj) =>
+  trimTemp(obj).map(({ state, temperature, ...el }) => ({
+    ...el,
+    state: upperCasingStates([state])[0],
+    temperature: farenheitToCelsius([temperature])[0],
+  }))
+
+const tempForecasts = (obj) => {
+  let data = formatObject(obj)
+  return data.map((el) => `${el.temperature}elsius in ${el.city}, ${el.state}`)
+}
