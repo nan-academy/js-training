1647201ddc4c03d543aa8b8bc572e6d82cb74db0
lee <lee-dasilva@hotmail.com>
2020-05-07 17:42:44 +0100

    rebasing

--- module/object.js
+++ module/object.js
@@ -1,7 +1,6 @@
 export const next = 'async'
 export const exercises = {
-<<<<<<< HEAD
-<<<<<<< HEAD
+  pick: { feedback: true },
   invert: { feedback: true },
   'molecules-cells': { feedback: true },
   fusion: { feedback: true },
@@ -10,18 +9,5 @@ export const exercises = {
   'deep-copy': { feedback: true },
   replica: { feedback: true },
   pronoun: { feedback: true },
-=======
-  pick: { feedback: true },
-=======
-  'pick-omit': { feedback: true },
->>>>>>> renaming
-  // pick / remove
-  // mapValue / filterValue / reduceValue on objects
-  // mapKey / filterKey / reduceKey on objects
-  // mapEntry / filterEntry / reduceEntry on objects
-  // assign / defaults
-  // assignDeep / defaultsDeep
-  // at 'a.0.x' { a: [{x: 1}] } -> 1 // at curry
->>>>>>> new exercise pick for the objects
   flags: { feedback: true },
 }
