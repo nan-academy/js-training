63379b84ca519659c9ea44cfec091e1e3ae6746e
lee <lee-dasilva@hotmail.com>
2020-05-07 16:31:37 +0100

    rebasing and solving conflicts

--- module/object.js
+++ module/object.js
@@ -3,6 +3,7 @@ export const exercises = {
   invert: { feedback: true },
   'molecules-cells': { feedback: true },
   fusion: { feedback: true },
+  'manipulate-keys': { feedback: true },
   'manipulate-entries': { feedback: true },
   'deep-copy': { feedback: true },
   replica: { feedback: true },
