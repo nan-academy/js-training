9043e2091791dc8dab21723d04930732dfccd809
Christopher Fremond <christopher.fremond@gmail.com>
2020-05-07 16:11:33 +0100

    1) change of name of `filter` to `using-filter` 2) change of name of `do your own filter` to `filter` 3) updated  `filter` (old `do your own filter`) with an extra test with the use of object 4) added `Object.getPrototypeOf([]).proto = ' [avoid for..in] '` line to avoid the use of for...in 5) updated frenchris solution of map to comply with the [avoid for..in]

--- tests/filter/pass_frenchris.js
+++ tests/filter/pass_frenchris.js
@@ -1,35 +1,13 @@
-const filterShortStateName = (arr) => arr.filter((word) => word.length < 7)
-
-const isVowel = (letter) =>
-  ['a', 'e', 'i', 'o', 'u'].includes(letter.toLowerCase())
-
-const filterStartVowel = (arr) => arr.filter((word) => isVowel(word[0]))
-
-const countVowels = (word) => {
-  let result = 0
-  for (let letter of word) if (isVowel(letter)) result++
-  return result
+const Filter = (el, fun) => {
+  let answer = []
+  for (const [i, val] of el.entries()) if (fun(val, i, el)) answer.push(val)
+  return answer
 }
 
-const filter5Vowels = (arr) => arr.filter((word) => countVowels(word) >= 5)
-
-const vowelsPresent = (word) => {
-  let charset = ['a', 'e', 'i', 'o', 'u']
-  let result = [...Array(5)]
-  for (let letter of word.toLowerCase())
-    for (let i = 0; i < charset.length; i++)
-      if (charset[i] === letter) result[i] = true
-  return result.filter((el) => el === true).length
+const Reject = (el, fun) => {
+  let answer = []
+  for (const [i, val] of el.entries()) if (!fun(val, i, el)) answer.push(val)
+  return answer
 }
 
-const filter1DistinctVowel = (arr) =>
-  arr.filter((word) => vowelsPresent(word) === 1)
-
-const multiFilter = (arr) =>
-  arr.filter(
-    (el) =>
-      el.capital.length > 7 &&
-      !isVowel(el.name[0]) &&
-      countVowels(el.tag) >= 1 &&
-      el.region !== 'South'
-  )
+const Partition = (el, fun) => [Filter(el, fun), Reject(el, fun)]
---
+++ tests/using-filter/pass_frenchris.js
@@ -0,0 +1,35 @@
+const filterShortStateName = (arr) => arr.filter((word) => word.length < 7)
+
+const isVowel = (letter) =>
+  ['a', 'e', 'i', 'o', 'u'].includes(letter.toLowerCase())
+
+const filterStartVowel = (arr) => arr.filter((word) => isVowel(word[0]))
+
+const countVowels = (word) => {
+  let result = 0
+  for (let letter of word) if (isVowel(letter)) result++
+  return result
+}
+
+const filter5Vowels = (arr) => arr.filter((word) => countVowels(word) >= 5)
+
+const vowelsPresent = (word) => {
+  let charset = ['a', 'e', 'i', 'o', 'u']
+  let result = [...Array(5)]
+  for (let letter of word.toLowerCase())
+    for (let i = 0; i < charset.length; i++)
+      if (charset[i] === letter) result[i] = true
+  return result.filter((el) => el === true).length
+}
+
+const filter1DistinctVowel = (arr) =>
+  arr.filter((word) => vowelsPresent(word) === 1)
+
+const multiFilter = (arr) =>
+  arr.filter(
+    (el) =>
+      el.capital.length > 7 &&
+      !isVowel(el.name[0]) &&
+      countVowels(el.tag) >= 1 &&
+      el.region !== 'South'
+  )
