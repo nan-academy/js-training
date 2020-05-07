78108313511329cdd4cbc042c1221a5b5ea69726
lee <lee-dasilva@hotmail.com>
2020-05-07 16:03:16 +0100

    solution: formatting

--- tests/neuron/pass_lee.js
+++ tests/neuron/pass_lee.js
@@ -6,11 +6,11 @@ const neuron = (series) => {
     let question = convertStr(groups[1])
     if (!acc[type]) acc[type] = {}
     if (!acc[type][question]) responses = []
-    
+
     responses.push(groups[3])
     acc[type][question] = {}
-    acc[type][question][type.slice(0, -1)] = groups[1],
-    acc[type][question]['responses']= responses
+    acc[type][question][type.slice(0, -1)] = groups[1]
+    acc[type][question]['responses'] = responses
     return acc
   }, {})
 }
