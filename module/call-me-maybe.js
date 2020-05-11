428a2fcf99768435fbe5f684b7d87cec57481b62
Clement Denis <le.mikmac@gmail.com>
2020-05-11 15:40:10 +0200

    call-me-maybe: change module order

--- module/call-me-maybe.js
+++ module/call-me-maybe.js
@@ -3,13 +3,12 @@ export const exercises = {
   'using-map': { feedback: true },
   'using-filter': { feedback: true },
   'using-reduce': { feedback: true },
-  'if-exists-then': { feedback: true },
   'for-each': { feedback: true },
   filter: { feedback: true },
+  quantifiers: { feedback: true },
   'sweet-curry': { feedback: true },
   'has-city': { feedback: true },
   flow: { feedback: true },
   currify: { feedback: true },
   'long-words': { feedback: true },
-
 }
