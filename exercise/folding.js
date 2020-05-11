c9dd2765d5799f583de1da2e18be4bc680b18943
Clement Denis <le.mikmac@gmail.com>
2020-05-11 18:41:50 +0200

    call-me-maybe/folding: add my solution + rework the subject

--- exercise/folding.js
+++ exercise/folding.js
@@ -1,19 +1,35 @@
 /*
-## Folding
+## Reduce
+
 ### Instructions
+
 Create three functions:
 - `fold` that receives an array, a function and an accumulator, in this order,
 and applies the function in the elements of the array starting on the left.
-- `foldr` that receives an array, a function and an accumulator, in this order,
+- `foldRight` that receives an array, a function and an accumulator, in this order,
 and applies the function in the elements of the array starting on the right.
-- `reducingArray` that works just like the method [].reduce. The arguments
-should be a function, an array and finally the accumulator if any.
-The accumulator in the three exercises is the starting value of the result.
+- `reduce` that works just like the method `[].reduce` when you don't
+specify an accumulator.
+The arguments should be an array and a function.
+The starting value of your accumulator must be the first value of the array.
+If your array is less than 1 argument you should throw an error.
+- `reduceRight` like reduce, from the last value to the first
+
 Example:
+```js
 const adder = (a,b) => a + b
-fold([1,2,3], adder, 2) //returns 8 (2 + 1 + 2 + 3)
-foldr([1,2,3], adder, 2) //returns 8 (2 + 3 + 2 + 1)
-The use of `reduce` and `reduceRight` is forbidden for this exercise.
+fold([1,2,3], adder, 2) // returns 8 (2 + 1 + 2 + 3)
+foldRight([1,2,3], adder, 2) // returns 8 (2 + 3 + 2 + 1)
+reduce([1,2,3], adder) // returns 6 (1 + 2 + 3)
+```
+
+The use of `[].reduce` and `[].reduceRight` is forbidden for this exercise.
+
+
+### Notions
+
+- https://devdocs.io/javascript/global_objects/array/reduce
+
 */
 Array.prototype.reduce = undefined
 Array.prototype.reduceRight = undefined
@@ -24,55 +40,41 @@ Array.prototype.reduceRight = undefined
 export const tests = []
 const t = (f) => tests.push(f)
 
-
-const arr = [3, 10, 26, 0]
-const arr1 = [4, 24, 10, 25]
-Object.freeze(arr)
-Object.freeze(arr1)
-
 const adder = (a, b) => a + b
 const multiplier = (a, b) => a * b
 const ifOdd = (a, b) => (b % 2 === 0 ? a + 1 : a - 1)
-
-t(({ eq }) => eq(fold(arr, adder, 0), 39))
-t(({ eq }) => eq(fold(arr, multiplier, 0), 0))
-t(({ eq }) => eq(fold(arr, ifOdd, 0), 2))
-t(({ eq }) => eq(fold(arr, adder, 4), 43))
-t(({ eq }) => eq(fold(arr, multiplier, 5), 0))
-t(({ eq }) => eq(fold(arr, ifOdd, 10), 12))
-
-t(({ eq }) => eq(foldr(arr, adder, 0), 39))
-t(({ eq }) => eq(foldr(arr, multiplier, 0), 0))
-t(({ eq }) => eq(foldr(arr, ifOdd, 0), 2))
-t(({ eq }) => eq(foldr(arr, adder, 4), 43))
-t(({ eq }) => eq(foldr(arr, multiplier, 5), 0))
-t(({ eq }) => eq(foldr(arr, ifOdd, 10), 12))
-
-const strArr1 = ['This ', 'is ', 'a ', 'simple', 'example']
-const strArr2 = 'The quick brown fox jumped over the lazy dog'
-  .split(' ')
-  .map((x) => (x += ' '))
-const objArr = [{ a: 12 }, { b: 6, c: { d: 2, e: 3 } }, { f: 'hello' }]
-Object.freeze(strArr1)
-Object.freeze(strArr2)
-Object.freeze(objArr)
-
 const concatenate = (a = '', b) => a.concat(b)
 const merger = (a, b) => ({ ...a, ...b })
 
-t(({ eq }) => eq(reducingArray(adder, arr), 39))
-t(({ eq }) => eq(reducingArray(adder, arr1), 63))
-t(({ eq }) =>
-  eq(reducingArray(concatenate, strArr1), 'This is a simpleexample')
+t(({ eq, ctx }) => eq(fold(ctx.num1, adder, 0), 39))
+t(({ eq, ctx }) => eq(fold(ctx.num1, multiplier, 0), 0))
+t(({ eq, ctx }) => eq(fold(ctx.num1, ifOdd, 0), 2))
+t(({ eq, ctx }) => eq(fold(ctx.num1, adder, 4), 43))
+t(({ eq, ctx }) => eq(fold(ctx.num1, multiplier, 5), 0))
+t(({ eq, ctx }) => eq(fold(ctx.num1, ifOdd, 10), 12))
+
+t(({ eq, ctx }) => eq(foldRight(ctx.num1, adder, 0), 39))
+t(({ eq, ctx }) => eq(foldRight(ctx.num1, multiplier, 0), 0))
+t(({ eq, ctx }) => eq(foldRight(ctx.num1, ifOdd, 0), 2))
+t(({ eq, ctx }) => eq(foldRight(ctx.num1, adder, 4), 43))
+t(({ eq, ctx }) => eq(foldRight(ctx.num1, multiplier, 5), 0))
+t(({ eq, ctx }) => eq(foldRight(ctx.num1, ifOdd, 10), 12))
+
+t(({ eq, ctx }) => eq(reduce(ctx.num1, adder), 39))
+t(({ eq, ctx }) => eq(reduce(ctx.num2, adder), 63))
+t(({ eq, ctx }) =>
+  eq(reduce(ctx.str1, concatenate), 'This is a simpleexample')
 )
-t(({ eq }) =>
+
+t(({ eq, ctx }) =>
   eq(
-    reducingArray(concatenate, strArr2),
+    reduce(ctx.str2, concatenate),
     'The quick brown fox jumped over the lazy dog '
   )
 )
-t(({ eq }) =>
-  eq(reducingArray(merger, objArr), {
+
+t(({ eq, ctx }) =>
+  eq(reduce(ctx.obj, merger), {
     a: 12,
     b: 6,
     c: { d: 2, e: 3 },
@@ -80,22 +82,24 @@ t(({ eq }) =>
   })
 )
 
-t(({ eq }) => eq(reducingArray(adder, arr, 5), 44))
-t(({ eq }) => eq(reducingArray(adder, arr1, 9), 72))
-t(({ eq }) =>
+t(({ eq, ctx }) => eq(reduce(ctx.num1, adder, 5), 44))
+t(({ eq, ctx }) => eq(reduce(ctx.num2, adder, 9), 72))
+t(({ eq, ctx }) =>
   eq(
-    reducingArray(concatenate, strArr1, ' right?'),
+    reduce(ctx.str1, concatenate, ' right?'),
     ' right?This is a simpleexample'
   )
 )
-t(({ eq }) =>
+
+t(({ eq, ctx }) =>
   eq(
-    reducingArray(concatenate, strArr2, ' The end'),
+    reduce(ctx.str2, concatenate, ' The end'),
     ' The endThe quick brown fox jumped over the lazy dog '
   )
 )
-t(({ eq }) =>
-  eq(reducingArray(merger, objArr, { ending: 3 }), {
+
+t(({ eq, ctx }) =>
+  eq(reduce(ctx.obj, merger, { ending: 3 }), {
     ending: 3,
     a: 12,
     b: 6,
@@ -103,4 +107,18 @@ t(({ eq }) =>
     f: 'hello',
   })
 )
+
 Object.freeze(tests)
+
+export const setup = () =>
+  Object.fromEntries(
+    Object.entries({
+      num1: [3, 10, 26, 0],
+      num2: [4, 24, 10, 25],
+      str1: ['This ', 'is ', 'a ', 'simple', 'example'],
+      str2: 'The quick brown fox jumped over the lazy dog'
+        .split(' ')
+        .map((x) => (x += ' ')),
+      obj: [{ a: 12 }, { b: 6, c: { d: 2, e: 3 } }, { f: 'hello' }],
+    }).map(([k, v]) => [k, Object.freeze(v)])
+  )
