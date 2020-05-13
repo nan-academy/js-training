22bcfbc08d80dff9126243d6b7f22508b3770554
lee <lee-dasilva@hotmail.com>
2020-05-13 10:41:12 +0100

    final exercise complit: sums

--- tests/sums/pass_lee.js
+++ tests/sums/pass_lee.js
@@ -1,47 +1,32 @@
-const getOnes = (nbr, arr) =>
-  nbr != 0 ? getOnes(nbr - 1, arr) && arr.push(1) && arr : []
-
 const sums = (nbr) => {
-  console.log()
-  let hip = getOnes(nbr, [])
-  let result = []
-  result.push(hip)
-
-  for (let i = 0; i < nbr-1; i++) {
-    console.log(i)
-    hip = [result[i][0] + result[i][1], ...getOnes(nbr - (2 + i), [])]
-    result.push(hip)
-  }
-  return result
+  if (!nbr) return []
+  let p = []
+  let last = 0 // index of last element in a partition
+  p[last] = nbr
+  return algo(nbr, p, last)
+    .slice(1)
+    .map((ele) => ele.sort())
 }
-// console.log(sums(0))
-// console.log(sums(1))
-// console.log(sums(2))
-console.log(sums(4))
-console.log(sums(7))
-/*
+const algo = (n, p, i, result = []) => {
+  result.push([...p])
+  let auxVal = 0
+  while (i >= 0 && p[i] === 1) {
+    auxVal += p[i]
+    i--
+  }
+  if (i < 0) return result
+  p[i]--
+  auxVal++
 
-nbr = 2
+  while (auxVal > p[i]) {
+    p[i + 1] = p[i]
+    auxVal = auxVal - p[i]
+    i++
+  }
 
-1 1
- 2
-
- nbr = 4
-
- 1 1 1 1  | 1 1 1 1
-  2 1 1   |  2 1 1
-   2 2    |   3 1
-    4     |    4
-
-  nbr = 7
- 1 1 1 1 1 1 1 | 1 1 1 1 1 1 1 |
-  2 1 1 1 1 1  |  2 1 1 1 1 1  |
-   3 1 1 1 1   |   2 2 1 1 1   | .......
-    4 1 1 1    |    2 2 2 1    | 3 2 1 1 | .....
-     5 1 1     |     4 2 1     |  3 2 2  | 3 3 1
-      6 1      |      2 5      |   3 4   |  3 4
-       7       |       7       |    7    |   7
-
-
-
-*/
+  p[i + 1] = auxVal
+  let pSize = p.reduce((a, c) => a + c)
+  p = pSize !== n ? p.slice(0, n - pSize) : p
+  i++
+  return algo(n, p, i, result)
+}
