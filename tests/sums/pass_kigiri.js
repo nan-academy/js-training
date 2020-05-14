7a6e0651f53f218c2823c8cf53bd1c5f01826f4d
Clement Denis <le.mikmac@gmail.com>
2020-05-14 17:14:03 +0200

    prettier

--- tests/sums/pass_kigiri.js
+++ tests/sums/pass_kigiri.js
@@ -4,7 +4,6 @@ const all = (n) =>
     .reduce((s, x) => [...s, [x], ...all(n - x).map((t) => [t, x])], [])
 
 const sums = (n) =>
-  [...new Set(all(n)
-    .map((n) => n.flat(Infinity).sort().join('.')))]
-    .map(n => n.split('.').map(Number))
+  [...new Set(all(n).map((n) => n.flat(Infinity).sort().join('.')))]
+    .map((n) => n.split('.').map(Number))
     .filter((r) => r.length > 1 && r.reduce((a, b) => a + b) === n)
