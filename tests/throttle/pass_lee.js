9d72cb405f343f885ee1e386621f0677296f0359
lee <lee-dasilva@hotmail.com>
2020-05-21 18:20:45 +0100

    full solution

--- tests/throttle/pass_lee.js
+++ tests/throttle/pass_lee.js
@@ -1,14 +1,21 @@
-// we will do this using __callbacks and setTimeout!__
 let timeOut = 0
-const throttle = (callback, limit = 0) => {
-  const a = () => {
+const throttle = (callback, waitLimit = 0, options = {}) => {
+  let context
+  options =
+    !Object.keys(options).length || options.trailing || options.leading
+      ? { leading: true, trailing: false }
+      : { ...options }
+  // With normal javascript functions, `this` is bound when the function is called.
+  // With arrow functions, `this` is bound to the context in which the function is originally created.
+  // so we must not use the arrow function
+  return function (...args) {
+    context = this
     if (!timeOut) {
-      timeOut = setTimeout(() => (timeOut = 0), limit)
-      return callback()
+      timeOut = setTimeout(() => {
+        if (options.trailing) callback.apply(context, args)
+        timeOut = 0
+      }, waitLimit)
+      if (!options.trailing && options.leading) callback.apply(context, args)
     }
   }
-  return a()
 }
-
-/// creating a function to pass in the throttling function
-const call = () => 'get me a drink'
