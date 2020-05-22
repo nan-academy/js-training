d9a8e69468253041e837b3ff0cca1eea49ef2624
lee <lee-dasilva@hotmail.com>
2020-05-22 15:55:32 +0100

    passing solution

--- tests/throttle/pass_lee.js
+++ tests/throttle/pass_lee.js
@@ -1,27 +1,26 @@
-const throttle = (callback, waitLimit = 0, options = {}) => {
+const throttle = (callback, waitLimit, options = {}) => {
+  let context, args, result
   let timeOut = 0
-  let beginning = options.leading && !options.trailing ? Date.now() : 0
-  options = !Object.keys(options).length
-    ? { leading: true, trailing: true }
-    : { ...options }
+  let beginning = 0
+  const timeEdge = () => {
+    beginning = options.leading ? Date.now() : 0
+    result = callback.apply(context, args)
+  }
   // With normal javascript functions, `this` is bound when the function is called.
   // With arrow functions, `this` is bound to the context in which the function is originally created.
   // so we must not use the arrow function here
-  return function (...args) {
+  return function () {
     let now = Date.now()
-    let remaining = (now - beginning) - waitLimit
-    let context = this
-    if (!timeOut) {
-      timeOut = setTimeout(() => {
-        if (
-          (options.trailing && !options.leading) ||
-          (!options.trailing && options.leading && !remaining)
-        )
-          callback.apply(context, args)
-        timeOut = 0
-      }, waitLimit)
-      if (options.trailing && options.leading) callback.apply(context, args)
+    if (!beginning && options.leading === false) beginning = now
+    let remaining = waitLimit - (now - beginning)
+    context = this
+    args = arguments
+    if (remaining <= 0 || remaining > waitLimit) {
+      beginning = now
+      result = callback.apply(context, args)
+    } else if (!timeOut && options.trailing) {
+      timeOut = setTimeout(timeEdge, remaining)
     }
+    return result
   }
 }
-
