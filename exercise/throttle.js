338e63bc7219ae84b8058cd1d0fcc21b98c310e5
lee <lee-dasilva@hotmail.com>
2020-05-22 12:16:11 +0100

    final version of the test

--- exercise/throttle.js
+++ exercise/throttle.js
@@ -4,9 +4,7 @@
 ### Instructions
 
 The objective of this exercise is to learn about `callbacks` and `setTimeout` for this
-you will have to create a function called `throttle` that works like the a normal throttle,
-just like `_.throttle` from lodash.
-
+you will have to create a function called `throttle` that works like `_.throttle` from lodash.
 
 ### Notions
 
@@ -19,32 +17,55 @@ just like `_.throttle` from lodash.
 export const tests = []
 const t = (f) => tests.push(f)
 
-const call = () => console.log('get me a drink')
-const callArgs = (str) => console.log(str)
+let arr = []
+const add = (el) => arr.push(el)
 
-const run = (callback, f, limit, callLimit, nbr) =>
+const run = (callback, callLimit, nbr) =>
   new Promise((r) => {
-    let a = []
-    let inter = setInterval(() => a.push(callback(f, limit)), callLimit)
+    let inter = setInterval(() => callback(1), callLimit)
     setTimeout(() => {
       clearInterval(inter)
-      r(a)
+      r(arr)
     }, callLimit * nbr)
   })
 
+// wait 26ms and execute 4 times every 16ms
 t(({ eq }) =>
-  run(throttle, call, 20, 10, 4).then((v) =>{
-    console.log(v)
-    return eq(v.filter((x) => x !== undefined).length, 2)
-  }
-    // eq(v.filter((x) => x !== undefined).length, 2)
+  run(throttle(add, 26), 16, 4).then((v) => eq(v.length, 2) && (arr = []))
+)
+// wait 26ms and execute 6 times every 16ms
+t(({ eq }) =>
+  run(throttle(add, 26), 16, 6).then((v) => eq(v.length, 3) && (arr = []))
+)
+// tests the trailing option
+t(({ eq }) =>
+  run(throttle(add, 26, { trailing: true, leading: false }), 16, 4).then(
+    (v) => eq(v.length, 1) && (arr = [])
   )
 )
-// t(({ eq }) =>
-//   run(throttle, call, 20, 10, 10).then((v) => {
-//     console.log(v)
-//     return eq(v.filter((x) => x !== undefined).length, 5)
-//   })
-// )
+// tests the leading option
+t(({ eq }) =>
+  run(throttle(add, 26, { trailing: false, leading: true }), 16, 4).then(
+    (v) => eq(v.length, 1) && (arr = [])
+  )
+)
+// tests with different time 10 times
+t(({ eq }) =>
+  run(throttle(add, 30), 20, 10).then(
+    (v) => eq(v.length, 6) && (arr = [])
+  )
+)
+// tests with both options false
+t(({ eq }) =>
+  run(throttle(add, 26, { trailing: false, leading: false }), 16, 2).then(
+    (v) => eq(v.length, 0) && (arr = [])
+  )
+)
+// tests with both options true
+t(({ eq }) =>
+  run(throttle(add, 26, { trailing: true, leading: true }), 16, 4).then(
+    (v) => eq(v.length, 2) && (arr = [])
+  )
+)
 
 Object.freeze(tests)
