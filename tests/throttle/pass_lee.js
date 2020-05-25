315ab05d776c23880f5bcb30a9fbcf0c3a17a9e9
lee <lee-dasilva@hotmail.com>
2020-05-25 12:03:25 +0100

    spliting throttle in two and rm "this" and "apply"

--- tests/throttle/pass_lee.js
+++ tests/throttle/pass_lee.js
@@ -1,25 +1,28 @@
-const throttle = (callback, waitLimit, options = {}) => {
-  let context, args, result
-  let timeOut = 0
-  let beginning = 0
-  const timeEdge = () => {
-    beginning = options.leading ? Date.now() : 0
-    result = callback.apply(context, args)
+let timeOut = 0
+const throttle = (callback, limit) => {
+  return (...args) => {
+    if (!timeOut) {
+      timeOut = setTimeout(() => (timeOut = 0), limit)
+      callback(...args)
+    }
   }
-  // With normal javascript functions, `this` is bound when the function is called.
-  // With arrow functions, `this` is bound to the context in which the function is originally created.
-  // so we must not use the arrow function here
-  return function () {
+}
+
+const opThrottle = (callback, waitLimit, options = {}) => {
+  let result
+  let beginning = 0
+  return (...args) => {
     let now = Date.now()
     if (!beginning && options.leading === false) beginning = now
     let remaining = waitLimit - (now - beginning)
-    context = this
-    args = arguments
     if (remaining <= 0 || remaining > waitLimit) {
       beginning = now
-      result = callback.apply(context, args)
+      result = callback(...args)
     } else if (!timeOut && options.trailing) {
-      timeOut = setTimeout(timeEdge, remaining)
+      timeOut = setTimeout(() => {
+        beginning = options.leading ? Date.now() : 0
+        result = callback(...args)
+      }, remaining)
     }
     return result
   }
