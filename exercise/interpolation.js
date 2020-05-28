2d7decad5e89c9139d2f8c9cd04aa0409e7a18cc
lee <lee-dasilva@hotmail.com>
2020-05-28 10:52:53 +0100

    test optimization

--- exercise/interpolation.js
+++ exercise/interpolation.js
@@ -3,11 +3,11 @@
 
 ### Instructions
 
-Create a function called `interpolation` that has as parameter `step`, `start`, `end`, `callback` and `duration`.
+Create a function called `interpolation` that has as parameters `step`, `start`, `end`, `callback` and `duration`.
 This function must calculate the interpolation points, (x, y), from the `start` position to `end` position depending
-on the number of `steps`. All the point must be calculated in the duration time.
+on the number of `steps`. All the points must be calculated in the duration time.
 
-For each interpolation point you must execute and pass as parameter to the callback the interpolation point ([x, y])
+For each interpolation point you must execute and pass as parameters to the callback the interpolation point ([x, y])
 
 ### Example
 
@@ -37,8 +37,7 @@ duration = 10
 
 ### Notions
 
-- https://developer.mozilla.org/en-US/docs/Web/API/WindowOrWorkerGlobalScope/setInterval
-
+- https://javascript.info/settimeout-setinterval
 
 // /*/ // ⚡
 
@@ -47,47 +46,49 @@ export const tests = []
 const t = (f) => tests.push(f)
 
 const add = (el, arr) => arr.push(el)
-
-// testing the amount of times the callback was called
-t(async ({ eq, wait }) => {
+const run = async (step, start, end, duration, waitTime, wait) => {
   let arr = []
-  interpolation(5, 0, 1, (ele) => add(ele, arr), 10)
-  await wait(15)
-  return eq(arr.length, 5)
+  interpolation(step, start, end, (ele) => add(ele, arr), duration)
+  await wait(waitTime)
+  return arr
+}
+
+// testing duration time, forbid loops
+t(async ({ eq, wait }) => eq((await run(5, 0, 4, 25, 14, wait)).length, 2))
+
+// testing duration time stamp
+t(async ({ eq }) => {
+  let arr = []
+  interpolation(5, 0, 4, (ele) => add(ele, arr), 10)
+  return eq(arr.length, 0)
 })
 
+// testing the amount of times the callback was called
+t(async ({ eq, wait }) => eq((await run(5, 0, 1, 10, 15, wait)).length, 5))
+
 // testing the interpolation points
-t(async ({ eq, wait }) => {
-  let arr = []
-  interpolation(5, 0, 1, (ele) => add(ele, arr), 10)
-  await wait(15)
-  return eq(arr, [
+t(async ({ eq, wait }) =>
+  eq(await run(5, 0, 1, 10, 15, wait), [
     [0, 2],
     [0.2, 4],
     [0.4, 6],
     [0.6, 8],
     [0.8, 10],
   ])
-})
+)
 
 // testing with different values
-t(async ({ eq, wait }) => {
-  let arr = []
-  interpolation(3, 1, 2, (ele) => add(ele, arr), 10)
-  await wait(15)
-  return eq(arr, [
+t(async ({ eq, wait }) =>
+  eq(await run(3, 1, 2, 10, 15, wait), [
     [1, 3.33],
     [1.33, 6.66],
     [1.67, 9.99],
   ])
-})
+)
 
 // testing with `duration` inferior to `step`
-t(async ({ eq, wait }) => {
-  let arr = []
-  interpolation(10, 2, 6, (ele) => add(ele, arr), 4)
-  await wait(15)
-  return eq(arr, [
+t(async ({ eq, wait }) =>
+  eq(await run(10, 2, 6, 4, 15, wait), [
     [2, 0.4],
     [2.4, 0.8],
     [2.8, 1.2],
@@ -99,21 +100,18 @@ t(async ({ eq, wait }) => {
     [5.2, 3.6],
     [5.6, 4],
   ])
-})
+)
 
 // testing with `start` superior to `end`
-// inverted straight
-t(async ({ eq, wait }) => {
-  let arr = []
-  interpolation(5, 6, 2, (ele) => add(ele, arr), 6)
-  await wait(15)
-  return eq(arr, [
+// inverted straight line
+t(async ({ eq, wait }) =>
+  eq(await run(5, 6, 2, 6, 10, wait), [
     [6, 1.2],
     [5.2, 2.4],
     [4.4, 3.6],
     [3.6, 4.8],
     [2.8, 6],
   ])
-})
+)
 
 Object.freeze(tests)
