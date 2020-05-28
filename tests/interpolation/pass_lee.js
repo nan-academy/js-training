132d030333836a4f7ddea42f5fdcafacd6f65796
lee <lee-dasilva@hotmail.com>
2020-05-28 10:52:53 +0100

    test optimization

--- tests/interpolation/pass_lee.js
+++ tests/interpolation/pass_lee.js
@@ -1,14 +1,18 @@
+const round = (nbr) => Math.round(nbr * 100) / 100
+
 const interpolation = (step, start, end, callback, duration) => {
   let fraction = (end - start) / step
   let timeStamp = duration / step
   let count = 0
   let countTime = 0
-  const interval = setInterval(() => {
-    let a = [
-      Math.round((start + count++ * fraction) * 100) / 100,
-      (countTime = Math.round((countTime + timeStamp) * 100) / 100),
-    ]
-    if (count >= step) clearInterval(interval)
-    callback(a)
-  }, timeStamp)
+  const interval = setInterval(
+    () =>
+      count >= step
+        ? clearInterval(interval)
+        : callback([
+            round(start + count++ * fraction),
+            (countTime = round(countTime + timeStamp)),
+          ]),
+    timeStamp
+  )
 }
