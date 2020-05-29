9a0ce670ae09835005ac900541d51010838f0f51
lee <lee-dasilva@hotmail.com>
2020-05-29 09:55:03 +0100

    fixed solution

--- tests/debounce/pass_lee.js
+++ tests/debounce/pass_lee.js
@@ -8,17 +8,10 @@ const debounce = (callback, wait) => {
 
 const opDebounce = (callback, wait, options = {}) => {
   let timeout = 0
-  let nothing = Object.entries(options).length && !options.leading
   return (...args) => {
     let callNow = options.leading && !timeout
     clearTimeout(timeout)
-
-    timeout = setTimeout(() => {
-      timeout = 0
-      if (!options.leading && !nothing) {
-        callback(...args)
-      }
-    }, wait)
-    if (callNow && !nothing) callback(...args)
+    timeout = setTimeout(() => callback(...args), wait)
+    if (callNow) callback(...args)
   }
 }
